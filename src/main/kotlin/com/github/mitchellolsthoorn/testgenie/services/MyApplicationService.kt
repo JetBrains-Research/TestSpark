3ba744fc4d87627f16dc8ff9eec64c4dfeefe8f1
GitHub Action <action@github.com>
2022-04-25 12:04:09 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/mitchellolsthoorn/testgenie/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.mitchellolsthoorn.testgenie.services
+
+import com.github.mitchellolsthoorn.testgenie.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
