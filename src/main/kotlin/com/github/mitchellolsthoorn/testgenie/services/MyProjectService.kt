e6d7fb0628eb9d51e6a9163bf5b28b1fcd45f0bf
GitHub Action <action@github.com>
2022-04-25 12:04:09 +0000

    Template cleanup

--- src/main/kotlin/com/github/mitchellolsthoorn/testgenie/services/MyProjectService.kt
+++ src/main/kotlin/com/github/mitchellolsthoorn/testgenie/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.mitchellolsthoorn.testgenie.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mitchellolsthoorn.testgenie.MyBundle
 
 class MyProjectService(project: Project) {
 
