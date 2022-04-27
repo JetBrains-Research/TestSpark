54a9cb28d1b5131a8626edc92f2455abda3ed9d0
Jegor Zelenjak <J.Zelenjak@student.tudelft.nl>
2022-04-27 19:38:06 +0200

    Change hand-made tool window panel to use the form file in order to simplify the creation of GUI for the tool window panel

--- src/main/kotlin/com/github/mitchellolsthoorn/testgenie/toolwindow/TestGenieToolWindowFactory.kt
+++ src/main/kotlin/com/github/mitchellolsthoorn/testgenie/toolwindow/TestGenieToolWindowFactory.kt
@@ -5,12 +5,16 @@ import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.content.Content
 import com.intellij.ui.content.ContentFactory
+import javax.swing.JTextArea
+import javax.swing.JTextField
 
 class TestGenieToolWindowFactory : ToolWindowFactory {
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-        val testGeniePanelWrapper = TestGeniePanelWrapper()
+        val testGeniePanelWrapper = TestGenieToolWindow(toolWindow)
         val contentFactory : ContentFactory = ContentFactory.SERVICE.getInstance()
-        val content : Content = contentFactory.createContent(testGeniePanelWrapper.getContent(), "TestGenie", false)
+        val content : Content = contentFactory.createContent(testGeniePanelWrapper.getContent(), "TestGenie1", false)
+
         toolWindow.contentManager.addContent(content)
+        toolWindow.contentManager.addContent(contentFactory.createContent(JTextArea("Here is where the hyperparameters will appear"), "Hyper-Parameters", false))
     }
 }
