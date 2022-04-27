dfd5d0b1ac865685cf12ad8762009958feeaffcd
Jegor Zelenjak <J.Zelenjak@student.tudelft.nl>
2022-04-28 00:45:48 +0200

    Make TestGenie class action appear only when some code is selected. Add a message dialog with the selected text

--- src/main/kotlin/com/github/mitchellolsthoorn/testgenie/actions/GenerateTestsForClassAction.kt
+++ src/main/kotlin/com/github/mitchellolsthoorn/testgenie/actions/GenerateTestsForClassAction.kt
@@ -7,18 +7,23 @@ import com.intellij.openapi.editor.CaretModel
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.ui.Messages
 
-
 class GenerateTestsForClassAction : AnAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
-        Messages.showErrorDialog(
+        val editor : Editor = e.getRequiredData(CommonDataKeys.EDITOR)
+        val selectedText : String? = editor.caretModel.currentCaret.selectedText
+
+        Messages.showInfoMessage(
             e.project,
-            "Generating tests for the class!" + e.dataContext.toString(),
+            "Generating tests for the class `${e.dataContext}`\nThe selected text is:\n$selectedText",
             "Generate Tests for Class"
         )
     }
 
-//    override fun update(e: AnActionEvent) {
-//        val editor: Editor = e.getRequiredData(CommonDataKeys.EDITOR)
-//    }
+    override fun update(e: AnActionEvent) {
+        val editor: Editor = e.getRequiredData(CommonDataKeys.EDITOR)
+        val caretModel : CaretModel = editor.caretModel
+
+        e.presentation.isEnabledAndVisible = caretModel.currentCaret.hasSelection()
+    }
 }
