cfa1631105f4aa766e6844bd18ae076c28b3b9e6
Martin Mladenov <m.mladenov@student.tudelft.nl>
2022-04-27 21:03:38 +0200

    Add save button for hyperparameters and listener

--- src/main/kotlin/com/github/mitchellolsthoorn/testgenie/toolwindow/TestGenieToolWindow.kt
+++ src/main/kotlin/com/github/mitchellolsthoorn/testgenie/toolwindow/TestGenieToolWindow.kt
@@ -1,7 +1,9 @@
 package com.github.mitchellolsthoorn.testgenie.toolwindow
 
+import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.wm.ToolWindow
 import java.awt.Dimension
+import javax.swing.JButton
 import javax.swing.JComboBox
 import javax.swing.JComponent
 import javax.swing.JLabel
@@ -17,8 +19,21 @@ class TestGenieToolWindow(_toolWindow: ToolWindow) {
     private var maxSizeTextField: JTextField? = null
     private var globalTimeOutTextField: JTextField? = null
     private var coverageCombobox: JComboBox<Boolean>? = null
+    private var saveButton: JButton? = null
 
     private var toolWindowPanel: JPanel? = null
+
+    init {
+        saveButton?.addActionListener {
+            Messages.showInfoMessage(
+                "Settings saved (not actually, still WIP)" +
+                        "\nmax_size: ${maxSizeTextField?.text}" +
+                        "\nglobal_timeout: ${globalTimeOutTextField?.text}" +
+                        "\ncoverage: ${coverageCombobox?.selectedItem}",
+                "Saved"
+            )
+        }
+    }
 
     fun getContent(): JComponent? {
         return toolWindowPanel!!
