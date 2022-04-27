b31dbd5f65f346a45a6bbabc22e0ca07bd5eda68
Lyuben Todorov <L.V.Todorov@student.tudelft.nl>
2022-04-27 17:28:13 +0300

    add experimental evosuite

--- src/main/kotlin/com/github/mitchellolsthoorn/testgenie/services/MyProjectService.kt
+++ src/main/kotlin/com/github/mitchellolsthoorn/testgenie/services/MyProjectService.kt
@@ -4,7 +4,6 @@ import com.intellij.openapi.project.Project
 import com.github.mitchellolsthoorn.testgenie.MyBundle
 
 class MyProjectService(project: Project) {
-
     init {
         println(MyBundle.message("projectService", project.name))
     }
