6305dd3f739500a1716fbd9c6399ea8ca32a874f
Jegor Zelenjak <J.Zelenjak@student.tudelft.nl>
2022-04-28 02:04:47 +0200

    Add very basic input validation for parameter texts fields

--- src/main/kotlin/com/github/mitchellolsthoorn/testgenie/toolwindow/TestGenieToolWindow.kt
+++ src/main/kotlin/com/github/mitchellolsthoorn/testgenie/toolwindow/TestGenieToolWindow.kt
@@ -3,6 +3,7 @@ package com.github.mitchellolsthoorn.testgenie.toolwindow
 import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.wm.ToolWindow
 import java.awt.Dimension
+import java.awt.event.ActionEvent
 import javax.swing.JButton
 import javax.swing.JComboBox
 import javax.swing.JComponent
@@ -24,18 +25,42 @@ class TestGenieToolWindow(_toolWindow: ToolWindow) {
     private var toolWindowPanel: JPanel? = null
 
     init {
-        saveButton?.addActionListener {
-            Messages.showInfoMessage(
-                "Settings saved (not actually, still WIP)" +
-                        "\nmax_size: ${maxSizeTextField?.text}" +
-                        "\nglobal_timeout: ${globalTimeOutTextField?.text}" +
-                        "\ncoverage: ${coverageCombobox?.selectedItem}",
-                "Saved"
-            )
-        }
+        saveButton?.addActionListener { addListenerForSaveButton(it) }
     }
 
     fun getContent(): JComponent? {
         return toolWindowPanel!!
+    }
+
+
+    private val addListenerForSaveButton : (ActionEvent) -> Unit = {
+        if (maxSizeTextField?.text == null || globalTimeOutTextField?.text == null) {
+            Messages.showErrorDialog("Please specify the value", "Empty Value Field")
+        } else {
+            // Validate all the input values
+            val maxSize: Int? = toInt(maxSizeTextField!!.text)
+            val globalTimeout: Int? = toInt(globalTimeOutTextField!!.text)
+            val coverage : Boolean = coverageCombobox?.selectedItem.toString().toBoolean()
+
+            if (maxSize == null || globalTimeout == null) {
+                Messages.showErrorDialog("Please specify a number", "Invalid Input Value")
+            } else {
+                Messages.showInfoMessage(
+                    "Settings saved (not actually, still WIP)" +
+                            "\nmax_size: $maxSize" +
+                            "\nglobal_timeout: $globalTimeout" +
+                            "\ncoverage: $coverage",
+                    "Saved"
+                )
+            }
+        }
+    }
+
+    private fun toInt(str: String): Int? {
+        return try {
+            str.toInt()
+        } catch (e : java.lang.NumberFormatException) {
+            null
+        }
     }
 }
