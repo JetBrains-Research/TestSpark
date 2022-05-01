b271e3db023ad0bb4931da42bda1f958efda02a0
Jegor Zelenjak <J.Zelenjak@student.tudelft.nl>
2022-05-01 10:11:25 +0200

    Change TestGenieSettingsConfigurable.kt class.
    Fixed a bug with checking if the entries in the UI elements of the settings page have been modified. The error was due to == instead of !=. Added extra methods reset, disposeUIResources, getPreferredFocusedComponent. Added "javadoc"

--- src/main/kotlin/com/github/mitchellolsthoorn/testgenie/settings/TestGenieSettingsConfigurable.kt
+++ src/main/kotlin/com/github/mitchellolsthoorn/testgenie/settings/TestGenieSettingsConfigurable.kt
@@ -3,35 +3,67 @@ package com.github.mitchellolsthoorn.testgenie.settings
 import com.intellij.openapi.options.Configurable
 import javax.swing.JComponent
 
+/**
+ * This class interacts with the other two Settings classes. It provides controller functionality for the settingsState.
+ */
 class TestGenieSettingsConfigurable : Configurable {
 
-    private var settingsComponent: TestGenieAppSettingsComponent = TestGenieAppSettingsComponent()
+    private var settingsComponent: TestGenieAppSettingsComponent? = null
 
+    /**
+     * Creates a settings component that holds the panel with the settings entries, and returns this panel
+     */
     override fun createComponent(): JComponent? {
-        val settings: TestGenieSettingsState = TestGenieSettingsService.getInstance().state!!
-        settingsComponent.userNameText = settings.userId
-        settingsComponent.ideaUserStatus = settings.ideaStatus
-        return settingsComponent.panel
+        settingsComponent = TestGenieAppSettingsComponent()
+        return settingsComponent!!.panel
     }
 
+    /**
+     * Sets the stored state values to the corresponding UI components. This method is called immediately after `createComponent` method.
+     */
+    override fun reset() {
+        val settingsState: TestGenieSettingsState = TestGenieSettingsService.getInstance().state!!
+        settingsComponent!!.userNameText = settingsState.userId
+        settingsComponent!!.ideaUserStatus = settingsState.ideaStatus
+    }
+
+    /**
+     * Checks if the values of the entries in the settings state are different from the persisted values of these entries.
+     */
     override fun isModified(): Boolean {
-        val settings: TestGenieSettingsState = TestGenieSettingsService.getInstance().state!!
-        var modified: Boolean = settingsComponent.userNameText == settings.userId
-        modified = modified or (settingsComponent.ideaUserStatus != settings.ideaStatus)
+        val settingsState: TestGenieSettingsState = TestGenieSettingsService.getInstance().state!!
+        var modified: Boolean = settingsComponent!!.userNameText != settingsState.userId
+        modified = modified or (settingsComponent!!.ideaUserStatus != settingsState.ideaStatus)
         return modified
     }
 
+    /**
+     * Persists the modified state after a user hit Apply button.
+     */
     override fun apply() {
-        val settings: TestGenieSettingsState = TestGenieSettingsService.getInstance().state!!
-        settings.userId = settingsComponent.userNameText!!
-        settings.ideaStatus = settingsComponent.ideaUserStatus
+        val settingsState: TestGenieSettingsState = TestGenieSettingsService.getInstance().state!!
+        settingsState.userId = settingsComponent!!.userNameText!!
+        settingsState.ideaStatus = settingsComponent!!.ideaUserStatus
     }
 
+    /**
+     * Returns the displayed name of the Settings tab.
+     */
     override fun getDisplayName(): String {
         return "AMOGUS"
     }
 
-//    override fun getPreferredFocusedComponent(): JComponent {
-//        return settingsComponent.myUserNameText
-//    }
+    /**
+     * Returns the UI component that should be focused when the page is opened.
+     */
+    override fun getPreferredFocusedComponent(): JComponent {
+        return settingsComponent!!.myUserNameText
+    }
+
+    /**
+     * Disposes the UI resources. It is called when a user closes the Settings dialog.
+     */
+    override fun disposeUIResources() {
+        settingsComponent = null
+    }
 }
