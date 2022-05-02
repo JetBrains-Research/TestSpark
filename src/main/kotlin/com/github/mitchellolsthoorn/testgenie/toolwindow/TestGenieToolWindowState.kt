c98eb227dfb17c5cdf1a260e45f75fa13f8e922f
Jegor Zelenjak <J.Zelenjak@student.tudelft.nl>
2022-05-02 17:52:11 +0200

    Add TestGenieToolWindowState.kt class
    Add TestGenieToolWindowState.kt class to store the state of the values of the parameters that appear in the tool window
    Co-authored-by: SergeyDatskiv S.Datskiv@student.tudelft.nl

---
+++ src/main/kotlin/com/github/mitchellolsthoorn/testgenie/toolwindow/TestGenieToolWindowState.kt
@@ -0,0 +1,40 @@
+package com.github.mitchellolsthoorn.testgenie.toolwindow
+
+/**
+ * This class is responsible for persisting the values of the parameters in the Quick Access,
+ *   which are also present in the settings menu.
+ */
+data class TestGenieToolWindowState
+    constructor(
+        var searchBudget: Int = 60,
+        var localSearchBudgetType: LocalSearchBudgetType = LocalSearchBudgetType.TIME,
+        var localSearchBudgetValue: Int = 5,
+        var stoppingCondition: StoppingCondition = StoppingCondition.MAXTIME,
+        var initializationTimeout: Int = 120,
+        var minimisationTimeout: Int = 60,
+        var assertionTimeout: Int = 60,
+        var junitCheckTimeout: Int = 60,
+        var population: Int = 50,
+        var populationLimit: PopulationLimit = PopulationLimit.INDIVIDUALS
+    )
+
+/**
+ * This enum contains the types for the search budget value parameter.
+ */
+enum class LocalSearchBudgetType {
+    TIME, STATEMENTS, TESTS, SUITES, FITNESS_EVALUATIONS
+}
+
+/**
+ * This enum contains the type for the stopping condition of the algorithm.
+ */
+enum class StoppingCondition {
+    MAXSTATEMENTS, MAXTESTS, MAXTIME, MAXGENERATIONS, MAXFITNESSEVALUATIONS, TIMEDELTA
+}
+
+/**
+ * This enum contains the type for the limit for the population size.
+ */
+enum class PopulationLimit {
+    INDIVIDUALS, TESTS, STATEMENTS
+}
