750d9a103bb00875841d87721b5096c32251114b
Sergey Datskiv <S.Datskiv@student.tudelft.nl>
2022-05-02 19:45:52 +0200

    Rework Sidebar Tool Window (10 Parameters)
    Rework Sidebar Tool Window to include 10 most frequently used/changed parameters. Also needed to delete TestGenieToolWindow.form.
    
    Co-Authored-By: Jegor Zelenjak <45541174+jzelenjak@users.noreply.github.com>

--- src/main/kotlin/com/github/mitchellolsthoorn/testgenie/toolwindow/TestGenieToolWindow.kt
+++ src/main/kotlin/com/github/mitchellolsthoorn/testgenie/toolwindow/TestGenieToolWindow.kt
@@ -5,11 +5,13 @@ import com.intellij.openapi.wm.ToolWindow
 import java.awt.Dimension
 import java.awt.event.ActionEvent
 import javax.swing.JButton
-import javax.swing.JComboBox
+import com.intellij.openapi.ui.ComboBox
+import com.intellij.ui.components.JBLabel
+import com.intellij.util.ui.FormBuilder
 import javax.swing.JComponent
-import javax.swing.JLabel
 import javax.swing.JPanel
-import javax.swing.JTextField
+import javax.swing.JSpinner
+import javax.swing.SpinnerNumberModel
 
 /**
  * This class stores the UI of the TestGenie tool window.
@@ -18,15 +20,35 @@ class TestGenieToolWindow(_toolWindow: ToolWindow) {
 
     private val toolWindow = _toolWindow
 
-    private var maxSizeTextField: JTextField? = null
-    private var globalTimeOutTextField: JTextField? = null
-    private var coverageCombobox: JComboBox<Boolean>? = null
+    private var searchBudget: JSpinner = JSpinner(SpinnerNumberModel(60,0,10000,1))
+    private var localSearchBudgetType: ComboBox<LocalSearchBudgetType> = ComboBox(LocalSearchBudgetType.values())
+    private var localSearchBudgetValue: JSpinner = JSpinner(SpinnerNumberModel(5,0,10000,1))
+    private var stoppingCondition: ComboBox<StoppingCondition> = ComboBox<StoppingCondition>(StoppingCondition.values())
+    private var initializationTimeout: JSpinner = JSpinner(SpinnerNumberModel(120,0,10000,1))
+    private var minimisationTimeout: JSpinner = JSpinner(SpinnerNumberModel(60,0,10000,1))
+    private var assertionTimeout: JSpinner = JSpinner(SpinnerNumberModel(60,0,10000,1))
+    private var junitCheckTimeout: JSpinner = JSpinner(SpinnerNumberModel(60,0,10000,1))
+    private var population: JSpinner = JSpinner(SpinnerNumberModel(5,0,10000,1))
+    private var populationLimit: ComboBox<PopulationLimit> = ComboBox(PopulationLimit.values())
     private var saveButton: JButton? = null
 
     private var toolWindowPanel: JPanel? = null
 
     init {
-        saveButton?.addActionListener { addListenerForSaveButton(it) }
+        toolWindowPanel = FormBuilder.createFormBuilder()
+                .addLabeledComponent(JBLabel("Search budget"), searchBudget, 1, false)
+                .addLabeledComponent(JBLabel("Local search budget type"), localSearchBudgetType, 1, false)
+                .addLabeledComponent(JBLabel("Local search budget value"), localSearchBudgetValue, 1, false)
+                .addLabeledComponent(JBLabel("Stopping condition"), stoppingCondition, 1, false)
+                .addLabeledComponent(JBLabel("Initialization timeout"), initializationTimeout, 1, false)
+                .addLabeledComponent(JBLabel("Minimisation timeout"), minimisationTimeout, 1, false)
+                .addLabeledComponent(JBLabel("Assertion timeout"), assertionTimeout, 1, false)
+                .addLabeledComponent(JBLabel("JUnit check timeout"), junitCheckTimeout, 1, false)
+                .addLabeledComponent(JBLabel("Population"), population, 1, false)
+                .addLabeledComponent(JBLabel("Population limit"), populationLimit, 1, false)
+                .addComponentFillVertically(JPanel(), 0)
+                .panel
+        //saveButton?.addActionListener { addListenerForSaveButton(it) }
     }
 
     /**
@@ -40,28 +62,28 @@ class TestGenieToolWindow(_toolWindow: ToolWindow) {
     /**
      * Adds a listener to the `Save` button to parse, validate and extract the entered values.
      */
-    private val addListenerForSaveButton : (ActionEvent) -> Unit = {
-        if (maxSizeTextField?.text == null || globalTimeOutTextField?.text == null) {
-            Messages.showErrorDialog("Please specify the value", "Empty Value Field")
-        } else {
-            // Validate all the input values
-            val maxSize: Int? = toInt(maxSizeTextField!!.text)
-            val globalTimeout: Int? = toInt(globalTimeOutTextField!!.text)
-            val coverage : Boolean = coverageCombobox?.selectedItem.toString().toBoolean()
-
-            if (maxSize == null || globalTimeout == null) {
-                Messages.showErrorDialog("Please specify a number", "Invalid Input Value")
-            } else {
-                Messages.showInfoMessage(
-                    "Settings saved (not actually, still WIP)" +
-                            "\nMax size: $maxSize" +
-                            "\nGlobal timeout: $globalTimeout" +
-                            "\nCoverage: $coverage",
-                    "Saved"
-                )
-            }
-        }
-    }
+//    private val addListenerForSaveButton : (ActionEvent) -> Unit = {
+//        if (maxSizeTextField?.text == null || globalTimeOutTextField?.text == null) {
+//            Messages.showErrorDialog("Please specify the value", "Empty Value Field")
+//        } else {
+//            // Validate all the input values
+//            val maxSize: Int? = toInt(maxSizeTextField!!.text)
+//            val globalTimeout: Int? = toInt(globalTimeOutTextField!!.text)
+//            val coverage : Boolean = coverageCombobox?.selectedItem.toString().toBoolean()
+//
+//            if (maxSize == null || globalTimeout == null) {
+//                Messages.showErrorDialog("Please specify a number", "Invalid Input Value")
+//            } else {
+//                Messages.showInfoMessage(
+//                    "Settings saved (not actually, still WIP)" +
+//                            "\nMax size: $maxSize" +
+//                            "\nGlobal timeout: $globalTimeout" +
+//                            "\nCoverage: $coverage",
+//                    "Saved"
+//                )
+//            }
+//        }
+//    }
 
     /**
      * Convert a string to an integer, or return null in case of an exception
