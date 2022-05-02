e672569eac7741a1605cafd7073fcb16bd6917b0
Martin Mladenov <m.mladenov@student.tudelft.nl>
2022-05-02 22:41:57 +0200

    Create TestCaseDisplayService

---
+++ src/main/kotlin/com/github/mitchellolsthoorn/testgenie/services/TestCaseDisplayService.kt
@@ -0,0 +1,8 @@
+package com.github.mitchellolsthoorn.testgenie.services;
+
+import com.intellij.openapi.project.Project
+import javax.swing.JPanel
+import javax.swing.JTextArea
+
+class TestCaseDisplayService(project: Project) {
+}
