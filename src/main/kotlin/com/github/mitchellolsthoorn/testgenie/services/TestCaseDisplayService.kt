a9e35b388766f7b79809d76c30d1b9ebc0553216
Martin Mladenov <m.mladenov@student.tudelft.nl>
2022-05-02 22:43:08 +0200

    Create displayTestCases method in test case display service

--- src/main/kotlin/com/github/mitchellolsthoorn/testgenie/services/TestCaseDisplayService.kt
+++ src/main/kotlin/com/github/mitchellolsthoorn/testgenie/services/TestCaseDisplayService.kt
@@ -7,4 +7,16 @@ import javax.swing.JTextArea
 class TestCaseDisplayService(project: Project) {
 
     val panel: JPanel = JPanel()
+
+    /**
+     * Fill the panel with the generated test cases. Remove all previously shown test cases.
+     *
+     * @param testCases The test cases to display
+     */
+    fun displayTestCases(testCases: List<String>) {
+        panel.removeAll()
+        testCases.forEach {
+            panel.add(JTextArea(it))
+        }
+    }
 }
