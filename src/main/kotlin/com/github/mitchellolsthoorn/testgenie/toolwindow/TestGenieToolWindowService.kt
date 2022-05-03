16e07b768ac9488b8871bd33eb141f93d7790fa8
Sergey Datskiv <S.Datskiv@student.tudelft.nl>
2022-05-03 22:51:55 +0200

    Add documention to TestGenieToolWindowService
    Add documentation to  TestGenieToolWindowService to cover the methods in that class, as well as class description.
    
    Co-Authored-By: Jegor Zelenjak <45541174+jzelenjak@users.noreply.github.com>

--- src/main/kotlin/com/github/mitchellolsthoorn/testgenie/toolwindow/TestGenieToolWindowService.kt
+++ src/main/kotlin/com/github/mitchellolsthoorn/testgenie/toolwindow/TestGenieToolWindowService.kt
@@ -5,17 +5,31 @@ import com.intellij.openapi.components.PersistentStateComponent
 import com.intellij.openapi.components.State
 import com.intellij.openapi.components.Storage
 
+/**
+ * This class is responsible for helping to implement persistence for Quick Access Tool Menu on a sidebar.
+ * It provides ability to load state and get state.
+ */
 @State(name = "TestGenieToolWindowState", storages = [Storage("testGenieToolWindowState.xml")])
 class TestGenieToolWindowService : PersistentStateComponent<TestGenieToolWindowState> {
 
     private var toolWindowState: TestGenieToolWindowState = TestGenieToolWindowState()
 
+    /**
+     * Gets the currently persisted state. This method is called every time the save button is pressed in the menu.
+     */
     override fun getState(): TestGenieToolWindowState? = toolWindowState
 
+    /**
+     * Loads the state. Everytime we need to update the parameters in the GUI according to a new state we call this
+     * method.
+     */
     override fun loadState(state: TestGenieToolWindowState) {
         toolWindowState = state
     }
 
+    /**
+     * Returns the service object with a static call.
+     */
     companion object {
         fun getInstance() : PersistentStateComponent<TestGenieToolWindowState> {
             return ApplicationManager.getApplication().getService(TestGenieToolWindowService::class.java)
