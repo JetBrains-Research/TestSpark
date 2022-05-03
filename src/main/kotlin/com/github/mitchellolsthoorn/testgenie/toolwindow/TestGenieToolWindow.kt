bc696794134f5c6ff20badcd54d2d8810da3f2de
Sergey Datskiv <S.Datskiv@student.tudelft.nl>
2022-05-03 23:02:16 +0200

    Refactor and add comment to TestGenieToolWindow
    Refactor code to make init a cleaner function. Add comments to the refactored code.
    
    Co-Authored-By: Jegor Zelenjak <45541174+jzelenjak@users.noreply.github.com>

--- src/main/kotlin/com/github/mitchellolsthoorn/testgenie/toolwindow/TestGenieToolWindow.kt
+++ src/main/kotlin/com/github/mitchellolsthoorn/testgenie/toolwindow/TestGenieToolWindow.kt
@@ -7,21 +7,12 @@ import javax.swing.JButton
 import com.intellij.openapi.ui.ComboBox
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBScrollPane
-import com.intellij.ui.table.JBTable
-import com.intellij.uiDesigner.core.AbstractLayout
-import com.intellij.util.containers.toArray
 import com.intellij.util.ui.FormBuilder
-import com.intellij.util.ui.GridBag
-import org.jdesktop.swingx.JXTitledSeparator
 import java.awt.*
-import javax.swing.BoxLayout
-import javax.swing.GroupLayout
 import javax.swing.JComponent
 import javax.swing.JLabel
 import javax.swing.JPanel
-import javax.swing.JScrollPane
 import javax.swing.JSpinner
-import javax.swing.JTable
 import javax.swing.SpinnerNumberModel
 
 /**
@@ -50,60 +41,63 @@ class TestGenieToolWindow(_toolWindow: ToolWindow) {
 
     init {
         loadState()
-        //saveButton.preferredSize = Dimension(40, 20)
-        //resetButton.preferredSize = Dimension(40, 20)
 
-//        val gb = GridBag()
-//            .setDefaultInsets(Insets(0, 0, AbstractLayout.DEFAULT_VGAP, AbstractLayout.DEFAULT_HGAP))
-//            //.setDefaultWeightX(1.0)
-//            .setDefaultAnchor(GridBagConstraints.WEST);
+        val buttons = createSaveAndResetButtons()
+        toolWindowPanel = createToolWindowPanel()
 
+        title.font = Font("Monochrome", Font.BOLD, 20)
+        resetButton.toolTipText = "Reset all parameters to default."
+        saveButton.addActionListener { addListenerForSaveButton(it) }
+        resetButton.addActionListener { addListenerForResetButton(it) }
+    }
+
+    /**
+     * Creates the entire tool window panel.
+     */
+    private fun createToolWindowPanel() = FormBuilder.createFormBuilder()
+            .setFormLeftIndent(30)
+            .addVerticalGap(25)
+            .addComponent(title)
+            .addLabeledComponent(customLabel("Search budget", "Maximum search duration."), searchBudget, 25, false)
+            .addTooltip("Default: 60 seconds")
+            .addLabeledComponent(customLabel("Local search budget type", "Interpretation of local search budget value."), localSearchBudgetType, 20, false)
+            .addTooltip("Default: Time")
+            .addLabeledComponent(customLabel("Local search budget value", "Maximum budget usable for improving individuals per local search."), localSearchBudgetValue, 20, false)
+            .addTooltip("Default: 5")
+            .addLabeledComponent(customLabel("Stopping condition", "What condition should be checked to end the search."), stoppingCondition, 20, false)
+            .addTooltip("Default: Max time")
+            .addLabeledComponent(customLabel("Initialization timeout", "Seconds allowed for initializing the search."), initializationTimeout, 20, false)
+            .addTooltip("Default: 120 seconds")
+            .addLabeledComponent(customLabel("Minimisation timeout", "Seconds allowed for minimization at the end."), minimisationTimeout, 20, false)
+            .addTooltip("Default: 60 seconds")
+            .addLabeledComponent(customLabel("Assertion timeout", "Seconds allowed for assertion generation at the end."), assertionTimeout, 20, false)
+            .addTooltip("Default: 60 seconds")
+            .addLabeledComponent(customLabel("JUnit check timeout", "Seconds allowed for checking the generated JUnit files <p/>(e.g., compilation and stability)."), junitCheckTimeout, 20, false)
+            .addTooltip("Default: 60 seconds")
+            .addLabeledComponent(customLabel("Population", "Population size of genetic algorithm."), population, 20, false)
+            .addTooltip("Default: 50")
+            .addLabeledComponent(customLabel("Population limit", "What to use as limit for the population size."), populationLimit, 20, false)
+            .addTooltip("Default: Individuals")
+            .addComponent(createSaveAndResetButtons())
+            //.addComponent(resetButton)
+            .addComponentFillVertically(JPanel(), 20)
+            .panel
+
+    /**
+     * Creates save and reset buttons and aligns them to the left of the tool window.
+     */
+    private fun createSaveAndResetButtons(): JPanel {
         val gbc = GridBagConstraints()
         gbc.anchor = GridBagConstraints.FIRST_LINE_START
         gbc.insets = Insets(10, 0, 10, 0)
 
-        val sus = JPanel(GridBagLayout())
-        sus.add(saveButton, gbc)
+        val buttons = JPanel(GridBagLayout())
+        buttons.add(saveButton, gbc)
         gbc.weightx = 1.0
-        sus.add(resetButton, gbc)
+        buttons.add(resetButton, gbc)
 
-        sus.preferredSize = Dimension(500, 30)
-
-
-        toolWindowPanel = FormBuilder.createFormBuilder()
-                .setFormLeftIndent(30)
-                .addVerticalGap(25)
-                .addComponent(title)
-                .addLabeledComponent(customLabel("Search budget", "Maximum search duration."), searchBudget, 25, false)
-                .addTooltip("Default: 60 seconds")
-                .addLabeledComponent(customLabel("Local search budget type", "Interpretation of local search budget value."), localSearchBudgetType, 20, false)
-                .addTooltip("Default: Time")
-                .addLabeledComponent(customLabel("Local search budget value", "Maximum budget usable for improving individuals per local search."), localSearchBudgetValue, 20, false)
-                .addTooltip("Default: 5")
-                .addLabeledComponent(customLabel("Stopping condition", "What condition should be checked to end the search."), stoppingCondition, 20, false)
-                .addTooltip("Default: Max time")
-                .addLabeledComponent(customLabel("Initialization timeout", "Seconds allowed for initializing the search."), initializationTimeout, 20, false)
-                .addTooltip("Default: 120 seconds")
-                .addLabeledComponent(customLabel("Minimisation timeout", "Seconds allowed for minimization at the end."), minimisationTimeout, 20, false)
-                .addTooltip("Default: 60 seconds")
-                .addLabeledComponent(customLabel("Assertion timeout", "Seconds allowed for assertion generation at the end."), assertionTimeout, 20, false)
-                .addTooltip("Default: 60 seconds")
-                .addLabeledComponent(customLabel("JUnit check timeout", "Seconds allowed for checking the generated JUnit files <p/>(e.g., compilation and stability)."), junitCheckTimeout, 20, false)
-                .addTooltip("Default: 60 seconds")
-                .addLabeledComponent(customLabel("Population", "Population size of genetic algorithm."), population, 20, false)
-                .addTooltip("Default: 50")
-                .addLabeledComponent(customLabel("Population limit", "What to use as limit for the population size."), populationLimit, 20, false)
-                .addTooltip("Default: Individuals")
-                .addComponent(sus)
-                //.addComponent(resetButton)
-                .addComponentFillVertically(JPanel(), 20)
-                .panel
-
-                title.font = Font("Monochrome", Font.BOLD, 20)
-
-                resetButton.toolTipText = "Reset all parameters to default."
-                saveButton.addActionListener { addListenerForSaveButton(it) }
-                resetButton.addActionListener { addListenerForResetButton(it) }
+        buttons.preferredSize = Dimension(500, 30)
+        return buttons
     }
 
     /**
@@ -179,16 +173,5 @@ class TestGenieToolWindow(_toolWindow: ToolWindow) {
         state.junitCheckTimeout = junitCheckTimeout.value as Int
         state.population = population.value as Int
         state.populationLimit = populationLimit.item
-    }
-
-    /**
-     * Convert a string to an integer, or return null in case of an exception
-     */
-    private fun toInt(str: String): Int? {
-        return try {
-            str.toInt()
-        } catch (e : java.lang.NumberFormatException) {
-            null
-        }
     }
 }
