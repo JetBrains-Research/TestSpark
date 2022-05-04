b005666ffc082e1071487fba50652076cceaa3cb
Lyuben Todorov <L.V.Todorov@student.tudelft.nl>
2022-05-04 12:40:44 +0300

    remove tmp path hardcode

--- src/main/kotlin/com/github/mitchellolsthoorn/testgenie/evo/EvoSuiteResultWatcher.kt
+++ src/main/kotlin/com/github/mitchellolsthoorn/testgenie/evo/EvoSuiteResultWatcher.kt
@@ -4,6 +4,7 @@ import com.google.gson.Gson
 import com.google.gson.stream.JsonReader
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.io.FileUtilRt
 import org.evosuite.utils.CompactReport
 import java.io.File
 import java.io.FileReader
@@ -13,17 +14,20 @@ class EvoSuiteResultWatcher(private val project: Project, private val resultPath
     private val log = Logger.getInstance(EvoSuiteResultWatcher::class.java)
 
     override fun run() {
-        val tmpDirPath = "/tmp/mincho/"
+        val sep = File.separatorChar
+        val testResultDirectory = "${FileUtilRt.getTempDirectory()}${sep}testGenieResults${sep}"
 
-        val tmpDir = File(tmpDirPath)
+        val tmpDir = File(testResultDirectory)
 
         if (!tmpDir.exists()) {
             tmpDir.mkdirs()
         }
 
+        log.info("Started result listener thread for $resultPath")
+
         while (true) {
             Thread.sleep(5000)
-            log.info("Searching for $resultPath results in $tmpDirPath")
+            log.info("Searching for $resultPath results in $testResultDirectory")
             val list = tmpDir.list()
             if (list == null) {
                 log.info("Empty dir")
@@ -33,7 +37,7 @@ class EvoSuiteResultWatcher(private val project: Project, private val resultPath
                         log.info("Found file $pathname")
 
                         val gson = Gson();
-                        val reader = JsonReader(FileReader("$tmpDirPath$pathname"))
+                        val reader = JsonReader(FileReader("$testResultDirectory$pathname"))
 
                         val testGenerationResult: CompactReport = gson.fromJson(reader, CompactReport::class.java);
 
