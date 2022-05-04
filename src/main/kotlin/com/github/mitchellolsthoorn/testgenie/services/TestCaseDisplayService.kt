6cd061b2545d3572d8f84ce71258a1ae1ef71a0a
Martin Mladenov <m.mladenov@student.tudelft.nl>
2022-05-04 12:22:19 +0200

    Add checkbox to test case display and make it look better

--- src/main/kotlin/com/github/mitchellolsthoorn/testgenie/services/TestCaseDisplayService.kt
+++ src/main/kotlin/com/github/mitchellolsthoorn/testgenie/services/TestCaseDisplayService.kt
@@ -1,12 +1,18 @@
 package com.github.mitchellolsthoorn.testgenie.services;
 
 import com.intellij.openapi.project.Project
-import javax.swing.JPanel
-import javax.swing.JTextArea
+import java.awt.BorderLayout
+import java.awt.Dimension
+import java.awt.GridLayout
+import javax.swing.*
 
 class TestCaseDisplayService(project: Project) {
 
     val panel: JPanel = JPanel()
+
+    init {
+        panel.layout = BoxLayout(panel, BoxLayout.Y_AXIS)
+    }
 
     /**
      * Fill the panel with the generated test cases. Remove all previously shown test cases.
@@ -16,7 +22,19 @@ class TestCaseDisplayService(project: Project) {
     fun displayTestCases(testCases: List<String>) {
         panel.removeAll()
         testCases.forEach {
-            panel.add(JTextArea(it))
+            val testCasePanel = JPanel()
+            testCasePanel.layout = BorderLayout()
+
+            testCasePanel.add(JCheckBox(), BorderLayout.WEST)
+
+            val editor = JTextArea(it)
+            editor.isEditable = false
+
+            testCasePanel.add(editor, BorderLayout.CENTER)
+
+            testCasePanel.maximumSize= Dimension(Short.MAX_VALUE.toInt(), testCasePanel.preferredSize.height)
+            panel.add(testCasePanel)
+            panel.add(Box.createRigidArea(Dimension(0, 5)))
         }
     }
 }
