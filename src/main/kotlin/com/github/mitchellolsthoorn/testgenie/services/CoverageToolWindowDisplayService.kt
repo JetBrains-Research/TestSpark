9d51f4e55627adcf0c8840ce2fb9aac42041a14b
Bolek Khodakov <b.a.khodakov@student.tudelft.nl>
2022-05-06 12:06:33 +0200

    Add layout logic to "coverage visualisation" window
    Add layout logic (skeleton) to "coverage visualisation" tab in toolwindow

---
+++ src/main/kotlin/com/github/mitchellolsthoorn/testgenie/services/CoverageToolWindowDisplayService.kt
@@ -0,0 +1,48 @@
+package com.github.mitchellolsthoorn.testgenie.services
+
+import com.intellij.openapi.project.Project
+import com.intellij.ui.components.JBLabel
+import com.intellij.ui.components.JBScrollPane
+import com.intellij.util.ui.FormBuilder
+import org.jdesktop.swingx.JXTitledSeparator
+import java.awt.BorderLayout
+import java.awt.Font
+import javax.swing.*
+
+class CoverageToolWindowDisplayService(private val project: Project) {
+    var mainPanel: JPanel ?= null
+    private var panelTitleAbsolute = JLabel("Absolute Test Coverage")
+    private var panelTitleRelative = JLabel("Relative Test Coverage")
+    private var absoluteLines = JBLabel("Amount of Lines covered: " + " Total: ")
+    private var absoluteBranch = JBLabel("Amount of Branches covered: " + " Total: ")
+    private var absoluteMutant = JBLabel("Amount of Mutants covered: "  + " Total: ")
+    private var relativeLines = JBLabel("Percentage of Lines covered: ")
+    private var relativeBranch = JBLabel("Percentage of Branches covered: ")
+    private var relativeMutant = JBLabel("Percentage of Mutants covered: ")
+
+    private var listLabels = listOf<JBLabel>(absoluteLines, absoluteBranch, absoluteMutant, relativeLines, relativeBranch, relativeMutant)
+
+
+
+    init {
+        panelTitleAbsolute.font = Font("Monochrome", Font.BOLD, 20)
+        panelTitleRelative.font = Font("Monochrome", Font.BOLD, 20)
+
+        for (listLabel in listLabels) {
+            listLabel.font = Font("Monochrome", Font.BOLD, 17)
+        }
+        mainPanel = FormBuilder.createFormBuilder()
+                .setFormLeftIndent(30)
+                .addVerticalGap(7)
+                .addComponent(panelTitleAbsolute,25)
+                .addComponent(absoluteLines, 20)
+                .addComponent(absoluteBranch, 20)
+                .addComponent(absoluteMutant, 20)
+                .addComponent(panelTitleRelative, 25)
+                .addComponent(relativeLines, 20)
+                .addComponent(relativeBranch, 20)
+                .addComponent(relativeMutant, 20)
+                .addComponentFillVertically(JPanel(), 20)
+                .panel
+    }
+}
