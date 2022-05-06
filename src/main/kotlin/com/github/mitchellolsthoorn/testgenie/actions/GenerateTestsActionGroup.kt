81412d0efd6cf086b71464b583430477b0485519
Jegor Zelenjak <J.Zelenjak@student.tudelft.nl>
2022-05-06 21:30:31 +0200

    Add GenerateTestsActionGroup to make the action group not visible in the menu if it has no visible children

--- src/main/kotlin/com/github/mitchellolsthoorn/testgenie/actions/GenerateTestsActionGroup.kt
+++ src/main/kotlin/com/github/mitchellolsthoorn/testgenie/actions/GenerateTestsActionGroup.kt
@@ -1,4 +1,18 @@
 package com.github.mitchellolsthoorn.testgenie.actions
 
-class GenerateTestsActionGroup {
+import com.intellij.openapi.actionSystem.DefaultActionGroup
+
+/**
+ * This class is the group class for TestGenieActions.
+ * It extends the default implementation simply to override `hideIfNoVisibleChildren` method
+ *   in order to make the group not visible in the editor popup menu if all actions are not visible.
+ */
+class GenerateTestsActionGroup : DefaultActionGroup() {
+
+    /**
+     * Makes the group not visible in the menu if it has no visible children.
+     */
+    override fun hideIfNoVisibleChildren(): Boolean {
+        return true
+    }
 }
