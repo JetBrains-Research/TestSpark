24f81d65a5f03800d6552c2081e167e4c70d4a54
Kiril Vasilev <k.v.vasilev-1@student.tudelft.nl>
2022-05-10 15:39:09 +0200

    Remove list coverage visualisation

--- src/main/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/services/CoverageToolWindowDisplayService.kt
+++ src/main/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/services/CoverageToolWindowDisplayService.kt
@@ -1,26 +1,14 @@
 package nl.tudelft.ewi.se.ciselab.testgenie.services
 
-import com.intellij.ui.components.JBLabel
 import com.intellij.ui.table.JBTable
 import com.intellij.util.ui.FormBuilder
-import java.awt.Font
-import javax.swing.JLabel
 import javax.swing.JPanel
 import javax.swing.table.AbstractTableModel
 
 class CoverageToolWindowDisplayService {
-    var mainPanel: JPanel ?= null
-    var panelTitleAbsolute = JLabel("Absolute Test Coverage")
-    var panelTitleRelative = JLabel("Relative Test Coverage")
-    var absoluteLines = JBLabel("Amount of Lines covered: " + " Total: ")
-    var absoluteBranch = JBLabel("Amount of Branches covered: " + " Total: ")
-    var absoluteMutant = JBLabel("Amount of Mutants covered: "  + " Total: ")
-    var relativeLines = JBLabel("Percentage of Lines covered: ")
-    var relativeBranch = JBLabel("Percentage of Branches covered: ")
-    var relativeMutant = JBLabel("Percentage of Mutants covered: ")
+    var mainPanel: JPanel? = null
     var data: ArrayList<String> = arrayListOf("Coverage", "Lines", "Branches", "Mutants", "", "", "", "")
 
-    private var listLabels = listOf(absoluteLines, absoluteBranch, absoluteMutant, relativeLines, relativeBranch, relativeMutant)
     // Implementation of abstract table model
     var tableModel = object : AbstractTableModel() {
         /**
@@ -59,24 +47,7 @@ class CoverageToolWindowDisplayService {
      * Show the labels for statistics on code coverage by tests in "coverage visualisation" tab
      */
     init {
-        panelTitleAbsolute.font = Font("Monochrome", Font.BOLD, 20)
-        panelTitleRelative.font = Font("Monochrome", Font.BOLD, 20)
-
-        for (listLabel in listLabels) {
-            listLabel.font = Font("Monochrome", Font.BOLD, 17)
-        }
-        mainPanel = FormBuilder.createFormBuilder()
-                .setFormLeftIndent(30)
-                .addVerticalGap(7)
-                .addComponent(panelTitleAbsolute,25)
-                .addComponent(absoluteLines, 20)
-                .addComponent(absoluteBranch, 20)
-                .addComponent(absoluteMutant, 20)
-                .addComponent(panelTitleRelative, 25)
-                .addComponent(relativeLines, 20)
-                .addComponent(relativeBranch, 20)
-                .addComponent(relativeMutant, 20)
-                .addComponentFillVertically(JPanel(), 20)
-                .panel
+        mainPanel =
+            FormBuilder.createFormBuilder().addComponent(table, 20).addComponentFillVertically(JPanel(), 20).panel
     }
 }
