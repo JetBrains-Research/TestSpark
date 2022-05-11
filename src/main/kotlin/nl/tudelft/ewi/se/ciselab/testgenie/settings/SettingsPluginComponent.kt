179330f779eb4d1bc59c8d4ebca56771cc271e13
Bolek Khodakov <b.a.khodakov@student.tudelft.nl>
2022-05-11 14:00:19 +0200

    Add description of Plugin to settings
    Plugin settings now has small description. EvoSuite settings tab now named "EvoSuite"

--- src/main/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/settings/SettingsPluginComponent.kt
+++ src/main/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/settings/SettingsPluginComponent.kt
@@ -1,8 +1,10 @@
 package nl.tudelft.ewi.se.ciselab.testgenie.settings
 
 import com.intellij.util.ui.FormBuilder
+import java.awt.Dimension
 import javax.swing.JCheckBox
 import javax.swing.JComponent
+import javax.swing.JLabel
 import javax.swing.JPanel
 
 /**
@@ -11,10 +13,19 @@ import javax.swing.JPanel
 class SettingsPluginComponent {
     var panel: JPanel? = null
 
+    //Plugin description
+    private val pluginDiscription = JLabel("<html><body>TestGenie is an external graphical IntelliJ plugin that integrates EvoSuite into the IDE." +
+            " EvoSuite is a tool that automatically generates test cases with assertions for classes written in Java code." +
+            " TestGenie makes this much easier, as it provides an intuitive modern interface for EvoSuite – so, no more CLI.")
+
     //Checkbox options
     private var showCoverageCheckBox = JCheckBox("Show visualised coverage")
 
     init {
+        //Apply style to panel (must be first)
+        stylizePanel()
+
+        //Create panel
         createSettingsPanel()
     }
 
@@ -23,11 +34,27 @@ class SettingsPluginComponent {
      */
     private fun createSettingsPanel() {
         panel = FormBuilder.createFormBuilder()
+                //Add description of TestGenie
+                .addComponent(pluginDiscription)
 
                 //Add visual coverage checkbox
                 .addComponent(showCoverageCheckBox, 10)
                 .addComponentFillVertically(JPanel(), 0)
                 .panel
+    }
+
+    /**
+     * Add stylistic additions to elements of Plugin settings panel (e.g. tooltips)
+     * IMPORTANT: this is responsible for wrapping the text of a label. It must be created before createSettingsPanel()
+     */
+    private fun stylizePanel() {
+
+        //Get dimensions of visible rectangle
+        val width = panel?.visibleRect?.width
+        val height = panel?.visibleRect?.height
+
+        //Set description text to wrap around dimensions
+        pluginDiscription.preferredSize = Dimension(width ?: 100, height ?: 100)
     }
 
     /**
