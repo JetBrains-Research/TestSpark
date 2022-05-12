a0acd9305bb645216ef44d81e8175a240002f583
Jegor Zelenjak <J.Zelenjak@student.tudelft.nl>
2022-05-12 16:44:39 +0200

    Fix indentation is toolwindow files using ktlint

--- src/main/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/toolwindow/TestGenieToolWindowFactory.kt
+++ src/main/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/toolwindow/TestGenieToolWindowFactory.kt
@@ -1,12 +1,12 @@
 package nl.tudelft.ewi.se.ciselab.testgenie.toolwindow
 
-import nl.tudelft.ewi.se.ciselab.testgenie.services.TestCaseDisplayService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.content.Content
 import com.intellij.ui.content.ContentFactory
+import nl.tudelft.ewi.se.ciselab.testgenie.services.TestCaseDisplayService
 
 /**
  * This class is responsible for creating the UI of the TestGenie tool window.
