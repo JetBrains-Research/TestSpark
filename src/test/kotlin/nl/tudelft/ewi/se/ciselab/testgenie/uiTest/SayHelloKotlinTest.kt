b6731fa48863f97d96f07e82faac1c48136a0cc4
Kiril Vasilev <k.v.vasilev-1@student.tudelft.nl>
2022-05-12 23:15:18 +0200

    Add custom timeouts for tests

--- src/test/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/uiTest/SayHelloKotlinTest.kt
+++ src/test/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/uiTest/SayHelloKotlinTest.kt
@@ -1,10 +1,10 @@
 package nl.tudelft.ewi.se.ciselab.testgenie.uiTest
 
-import nl.tudelft.ewi.se.ciselab.testgenie.uiTest.pages.WelcomeFrame
-import nl.tudelft.ewi.se.ciselab.testgenie.uiTest.utils.RemoteRobotExtension
 import com.intellij.remoterobot.RemoteRobot
 import nl.tudelft.ewi.se.ciselab.testgenie.uiTest.pages.IdeaFrame
 import nl.tudelft.ewi.se.ciselab.testgenie.uiTest.pages.SettingsFrame
+import nl.tudelft.ewi.se.ciselab.testgenie.uiTest.pages.WelcomeFrame
+import nl.tudelft.ewi.se.ciselab.testgenie.uiTest.utils.RemoteRobotExtension
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeAll
@@ -13,18 +13,17 @@ import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.extension.ExtendWith
 import java.time.Duration
 
-// The code here was copied from JetBrains/intellij-ui-test-robot library, in order to experiment with the UI testing.
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ExtendWith(RemoteRobotExtension::class)
 class SayHelloKotlinTest {
 
     @BeforeAll
     fun setUpAll(remoteRobot: RemoteRobot): Unit = with(remoteRobot) {
-        find(WelcomeFrame::class.java).apply {
+        find(WelcomeFrame::class.java, timeout = Duration.ofSeconds(15)).apply {
             open("untitled")
         }
 
-        find(IdeaFrame::class.java).apply {
+        find(IdeaFrame::class.java, timeout = Duration.ofSeconds(15)).apply {
             openSettings()
         }
     }
@@ -36,11 +35,11 @@ class SayHelloKotlinTest {
 
     @AfterAll
     fun closeAll(remoteRobot: RemoteRobot): Unit = with(remoteRobot) {
-        find(SettingsFrame::class.java).apply {
+        find(SettingsFrame::class.java, timeout = Duration.ofSeconds(15)).apply {
             closeSettings()
         }
 
-        find(IdeaFrame::class.java).apply {
+        find(IdeaFrame::class.java, timeout = Duration.ofSeconds(15)).apply {
             closeProject()
         }
     }
