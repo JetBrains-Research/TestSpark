8927b40368aeedb1886c1bf8ab2f6cbdcdfb8711
Martin Mladenov <m.mladenov@student.tudelft.nl>
2022-05-12 22:28:35 +0200

    Add method descriptor generation for arrays and objects

--- src/main/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/helpers/MethodDescriptorHelper.kt
+++ src/main/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/helpers/MethodDescriptorHelper.kt
@@ -1,10 +1,13 @@
 package nl.tudelft.ewi.se.ciselab.testgenie.helpers
 
+import com.intellij.psi.PsiClassType
 import com.intellij.psi.PsiMethod
 import com.intellij.psi.PsiSubstitutor
 import com.intellij.psi.PsiType
 import com.intellij.util.containers.stream
 import java.util.stream.Collectors
+
+// Grammar taken from: https://docs.oracle.com/javase/specs/jvms/se11/html/jvms-4.html#jvms-4.3
 
 fun generateMethodDescriptor(psiMethod: PsiMethod): String {
     val parameterTypes = psiMethod.getSignature(PsiSubstitutor.EMPTY)
@@ -28,7 +31,25 @@ fun generateReturnDescriptor(psiMethod: PsiMethod): String {
 }
 
 fun generateFieldType(psiType: PsiType): String {
+    // arrays (ArrayType)
+    if (psiType.arrayDimensions > 0) {
+        val arrayType = generateFieldType(psiType.deepComponentType)
+        return "[".repeat(psiType.arrayDimensions) + arrayType
+    }
 
+    //  objects (ObjectType)
+    if (psiType is PsiClassType) {
+        val classType = psiType.resolve()
+        if (classType != null) {
+            val className = classType.qualifiedName?.replace('.', '/')
+
+            // no need to handle generics: they are not part of method descriptors
+
+            return "L$className;"
+        }
+    }
+
+    // primitives (BaseType)
     psiType.canonicalText.let {
         return when (it) {
             "int" -> "I"
@@ -39,7 +60,7 @@ fun generateFieldType(psiType: PsiType): String {
             "byte" -> "B"
             "char" -> "C"
             "short" -> "S"
-            else -> "L$it;" // todo: fix
+            else -> throw IllegalArgumentException("Unknown type: $it")
         }
     }
 }
