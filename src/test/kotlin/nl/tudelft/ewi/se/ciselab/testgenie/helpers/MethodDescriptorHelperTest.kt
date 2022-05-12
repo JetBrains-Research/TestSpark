b90a59f94586f2a540866d0987ee9ef6445abdd0
Martin Mladenov <m.mladenov@student.tudelft.nl>
2022-05-12 18:53:02 +0200

    Add test for primitive types

--- src/test/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/helpers/MethodDescriptorHelperTest.kt
+++ src/test/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/helpers/MethodDescriptorHelperTest.kt
@@ -1,10 +1,35 @@
 package nl.tudelft.ewi.se.ciselab.testgenie.helpers
 
 import com.intellij.psi.impl.PsiJavaParserFacadeImpl
-import org.junit.jupiter.api.Test
 
 import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 class MethodDescriptorHelperTest {
 
+    @ParameterizedTest
+    @MethodSource("primitiveFieldTypeTestGenerator")
+    fun primitiveFieldTypeTest(text: String, expected: String) {
+        val psiType = PsiJavaParserFacadeImpl.getPrimitiveType(text)
+        assertThat(generateFieldType(psiType)).isEqualTo(expected);
+    }
+
+    companion object {
+        @JvmStatic
+        private fun primitiveFieldTypeTestGenerator(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of("int", "I"),
+                Arguments.of("long", "J"),
+                Arguments.of("float", "F"),
+                Arguments.of("double", "D"),
+                Arguments.of("boolean", "Z"),
+                Arguments.of("byte", "B"),
+                Arguments.of("char", "C"),
+                Arguments.of("short", "S"),
+            )
+        }
+    }
 }
