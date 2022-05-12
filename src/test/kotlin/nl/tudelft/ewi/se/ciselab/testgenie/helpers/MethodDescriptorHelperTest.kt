04429878156ebabf9ffe89365de91972c46e060c
Martin Mladenov <m.mladenov@student.tudelft.nl>
2022-05-12 18:43:07 +0200

    Add basic test structure

---
+++ src/test/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/helpers/MethodDescriptorHelperTest.kt
@@ -0,0 +1,10 @@
+package nl.tudelft.ewi.se.ciselab.testgenie.helpers
+
+import com.intellij.psi.impl.PsiJavaParserFacadeImpl
+import org.junit.jupiter.api.Test
+
+import org.assertj.core.api.Assertions.*
+
+class MethodDescriptorHelperTest {
+
+}
