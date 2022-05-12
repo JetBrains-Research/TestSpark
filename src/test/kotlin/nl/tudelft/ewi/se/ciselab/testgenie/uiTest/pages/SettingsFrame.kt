c49cde18822bce858a124148a7e7905456b1a551
Kiril Vasilev <k.v.vasilev-1@student.tudelft.nl>
2022-05-12 23:30:50 +0200

    Add test to verify existence of introduction label

--- src/test/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/uiTest/pages/SettingsFrame.kt
+++ src/test/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/uiTest/pages/SettingsFrame.kt
@@ -23,6 +23,21 @@ class SettingsFrame(remoteRobot: RemoteRobot, remoteComponent: RemoteComponent) 
     private val closeSettingsAction
         get() = actionLink(byXpath("//div[@class='DialogHeader']//div[@class='JButton']"))
 
+    // Action for search text filed in Settings menu
+    private val searchTextBox
+        get() = textField(byXpath("//div[@class='SettingsSearch']//div[@class='TextFieldWithProcessing']"))
+
+    // Action for introduction label
+    val introLabel
+        get() = jLabel(byXpath("//div[@accessiblename='TestGenie is an external graphical IntelliJ plugin that integrates EvoSuite into the IDE. EvoSuite is a tool that automatically generates test cases with assertions for classes written in Java code. TestGenie makes this much easier, as it provides an intuitive modern interface for EvoSuite – so, no more CLI.' and @class='JLabel' and @text='<html><body>TestGenie is an external graphical IntelliJ plugin that integrates EvoSuite into the IDE. EvoSuite is a tool that automatically generates test cases with assertions for classes written in Java code. TestGenie makes this much easier, as it provides an intuitive modern interface for EvoSuite – so, no more CLI.']"))
+
+    /**
+     * Search for TestGenie in Settings.
+     */
+    fun findTestGenie() {
+        searchTextBox.text = "TestGenie"
+    }
+
     /**
      * Method to open the settings of IntelliJ.
      */
