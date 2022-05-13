1fcd444c3fdba81774f335771ace6e9f328c35d3
Sergey Datskiv <S.Datskiv@student.tudelft.nl>
2022-05-13 22:11:05 +0200

    Add comments to ToolWindowFrame.kt values
    Add comments and space formatting to ToolWindowFrame.kt values.
    
    Co-Authored-By: Jegor Zelenjak <45541174+jzelenjak@users.noreply.github.com>

--- src/test/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/uiTest/pages/ToolWindowFrame.kt
+++ src/test/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/uiTest/pages/ToolWindowFrame.kt
@@ -25,32 +25,60 @@ class ToolWindowFrame(remoteRobot: RemoteRobot, remoteComponent: RemoteComponent
     // The title "Quick Access Parameters"
     val title
         get() = jLabel(byXpath("//div[@javaclass='javax.swing.JLabel']"))
+
+    // The search budget separator line
     val searchBudgetSeparator
         get() = jLabel(byXpath("//div[@accessiblename='Search budget' and @class='JLabel' and @text='Search budget']"))
+
+    // The search budget type text
     val searchBudgetType
         get() = jLabel(byXpath("//div[@text='Search budget type']"))
+
+    // The search buget value text
     val searchBudgetValue
         get() = jLabel(byXpath("//div[@accessiblename='Search budget' and @class='JBLabel' and @text='Search budget']"))
+
+    // The timeouts' separator line
     val timeoutsSeparator
         get() = jLabel(byXpath("//div[@text='Timeouts']"))
+
+    // The initialization timeout text
     val initializationTimeout
         get() = jLabel(byXpath("//div[@text='Initialization timeout']"))
+
+    // The minimisation timeout text
     val minimisationTimeout
         get() = jLabel(byXpath("//div[@text='Minimisation timeout']"))
+
+    // The assertion timeout text
     val assertionTimeout
         get() = jLabel(byXpath("//div[@text='Assertion timeout']"))
+
+    // The JUnit check timeout text
     val jUnitCheckTimeout
         get() = jLabel(byXpath("//div[@text='JUnit check timeout']"))
+
+    // The genetic algorithm separator line
     val geneticAlgorithmSeparator
         get() = jLabel(byXpath("//div[@text='Genetic Algorithm']"))
+
+    // The population limit text
     val populationLimit
         get() = jLabel(byXpath("//div[@text='Population limit']"))
+
+    // The population value text
     val populationValue
         get() = jLabel(byXpath("//div[@text='Population']"))
+
+    // The action link text
     val actionLink
         get() = actionLink(byXpath("//div[@class='ActionLink']"))
+
+    // The save button
     val saveButton
         get() = button(byXpath("//div[@text='Save']"))
+
+    // The reset button
     val resetButton
         get() = button(byXpath("//div[@text='Reset']"))
 
