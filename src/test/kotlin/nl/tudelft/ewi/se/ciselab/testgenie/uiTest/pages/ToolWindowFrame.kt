768d3b4da3daa96fb1326d8bd76942087dbf2093
Jegor Zelenjak <J.Zelenjak@student.tudelft.nl>
2022-05-13 20:47:56 +0200

    Fix the first QuickAccessParameters test
    Co-Authored-By: s.datskiv@student.tudelft.nl

--- src/test/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/uiTest/pages/ToolWindowFrame.kt
+++ src/test/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/uiTest/pages/ToolWindowFrame.kt
@@ -8,14 +8,28 @@ import com.intellij.remoterobot.fixtures.FixtureName
 import com.intellij.remoterobot.search.locators.byXpath
 
 @FixtureName("Tool Window Frame")
-@DefaultXpath("type", "//div[@class='BorderLayoutPanel'][.//div[@class='BaseLabel']]")
+@DefaultXpath(
+    "type",
+    "//div[@accessiblename='Generated Tests Tool Window']"
+) // TODO: rename to "Parameters Tool Window Frame" when we remove "Generated Tests" tab
 class ToolWindowFrame(remoteRobot: RemoteRobot, remoteComponent: RemoteComponent) :
     CommonContainerFixture(remoteRobot, remoteComponent) {
 
+    // The tab label "Parameters"
     val parametersTab
         get() = actionLink(byXpath("//div[@text='Parameters']"))
 
-    fun findQuickAccessParameters() {
+    // The actual panel
+    val quickAccessParametersContent
+        get() = actionLink(byXpath("//div[@class='DumbUnawareHider']"))
+
+    // The title "Quick Access Parameters"
+    val title
+        get() = jLabel(byXpath("//div[@javaclass='javax.swing.JLabel']"))
+    /**
+     * Clicks on the "Parameters" tab to open the panel with parameters.
+     */
+    fun openQuickAccessParametersTab() {
         parametersTab.click()
     }
 }
