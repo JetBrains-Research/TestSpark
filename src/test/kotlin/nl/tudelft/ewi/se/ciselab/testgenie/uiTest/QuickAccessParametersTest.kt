fd5b8e77206184429d48a124d7c58d70d3827b73
Sergey Datskiv <S.Datskiv@student.tudelft.nl>
2022-05-13 17:03:23 +0200

    Add files for tool window testing
    Add files for tool window testing. They do not work, however, they are there to experiment with.
    
    Co-Authored-By: Jegor Zelenjak <45541174+jzelenjak@users.noreply.github.com>

--- src/test/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/uiTest/QuickAccessParametersTest.kt
+++ src/test/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/uiTest/QuickAccessParametersTest.kt
@@ -2,14 +2,24 @@ package nl.tudelft.ewi.se.ciselab.testgenie.uiTest
 
 import com.intellij.remoterobot.RemoteRobot
 import nl.tudelft.ewi.se.ciselab.testgenie.uiTest.pages.IdeaFrame
+import nl.tudelft.ewi.se.ciselab.testgenie.uiTest.pages.QuickAccessParametersFrame
 import nl.tudelft.ewi.se.ciselab.testgenie.uiTest.pages.SettingsFrame
 import nl.tudelft.ewi.se.ciselab.testgenie.uiTest.pages.WelcomeFrame
+import nl.tudelft.ewi.se.ciselab.testgenie.uiTest.utils.RemoteRobotExtension
 import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.MethodOrderer
 import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.TestMethodOrder
+import org.junit.jupiter.api.extension.ExtendWith
 import java.time.Duration
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(RemoteRobotExtension::class)
 class QuickAccessParametersTest {
     @BeforeAll
     fun setUpAll(remoteRobot: RemoteRobot): Unit = with(remoteRobot) {
@@ -19,13 +29,16 @@ class QuickAccessParametersTest {
 
         find(IdeaFrame::class.java, timeout = Duration.ofSeconds(15)).apply {
             waitForBackgroundTasks()
-            openSettings()
+            openToolWindow()
         }
     }
 
     @Order(1)
     @Test
     fun checkTestGenieToolWindowPanel(remoteRobot: RemoteRobot) = with(remoteRobot) {
+        val quickAccessParametersFrame = find(QuickAccessParametersFrame::class.java, timeout = Duration.ofSeconds(15))
+        quickAccessParametersFrame.openQuickAccessParameters()
+        Assertions.assertTrue(quickAccessParametersFrame.titleGetFindJLabel.isShowing)
     }
 
     @AfterAll
