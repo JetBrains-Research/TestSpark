093eaeac04b0b68640e7480aafe07ee7673b525d
Jegor Zelenjak <J.Zelenjak@student.tudelft.nl>
2022-05-13 20:47:56 +0200

    Fix the first QuickAccessParameters test
    Co-Authored-By: s.datskiv@student.tudelft.nl

--- src/test/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/uiTest/QuickAccessParametersTest.kt
+++ src/test/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/uiTest/QuickAccessParametersTest.kt
@@ -6,6 +6,7 @@ import nl.tudelft.ewi.se.ciselab.testgenie.uiTest.pages.ToolWindowFrame
 import nl.tudelft.ewi.se.ciselab.testgenie.uiTest.pages.WelcomeFrame
 import nl.tudelft.ewi.se.ciselab.testgenie.uiTest.utils.RemoteRobotExtension
 import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.MethodOrderer
 import org.junit.jupiter.api.Order
@@ -26,22 +27,26 @@ class QuickAccessParametersTest {
      */
     @BeforeAll
     fun setUpAll(remoteRobot: RemoteRobot): Unit = with(remoteRobot) {
+        // Open the 'untitled' project
         find(WelcomeFrame::class.java, timeout = Duration.ofSeconds(15)).apply {
             open("untitled")
         }
 
+        // Open the TestGenie tool window
         find(IdeaFrame::class.java, timeout = Duration.ofSeconds(15)).apply {
             clickOnToolWindow()
-        }
-
-        find(ToolWindowFrame::class.java, timeout = Duration.ofSeconds(15)).apply {
-            findQuickAccessParameters()
         }
     }
 
     @Order(1)
     @Test
     fun checkTestGenieToolWindowPanel(remoteRobot: RemoteRobot) = with(remoteRobot) {
+        // Open the tool window frame
+        val toolWindowFrame = find(ToolWindowFrame::class.java, timeout = Duration.ofSeconds(15))
+        // Open the "Quick Access Parameters" tab
+        toolWindowFrame.openQuickAccessParametersTab()
+        // toolWindowFrame.quickAccessParametersContent.click()
+        Assertions.assertTrue(toolWindowFrame.title.isVisible())
     }
 
     /**
