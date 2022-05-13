787874ef7701ac1be45079ca3f61e5765dc10d64
Sergey Datskiv <S.Datskiv@student.tudelft.nl>
2022-05-13 17:36:25 +0200

    UI testing, setup TestGenie sidebar
    Setup the BeforeAll to open the untitled project and to open the TestGenie tool window on the sidebar.
    Also, AfterAll to close the tool window by clicking on the sidebar strip button and close the project, thus bringing the IntelliJ back to welcome screen.
    
    Co-Authored-By: Jegor Zelenjak <45541174+jzelenjak@users.noreply.github.com>

--- src/test/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/uiTest/QuickAccessParametersTest.kt
+++ src/test/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/uiTest/QuickAccessParametersTest.kt
@@ -2,12 +2,9 @@ package nl.tudelft.ewi.se.ciselab.testgenie.uiTest
 
 import com.intellij.remoterobot.RemoteRobot
 import nl.tudelft.ewi.se.ciselab.testgenie.uiTest.pages.IdeaFrame
-import nl.tudelft.ewi.se.ciselab.testgenie.uiTest.pages.QuickAccessParametersFrame
-import nl.tudelft.ewi.se.ciselab.testgenie.uiTest.pages.SettingsFrame
 import nl.tudelft.ewi.se.ciselab.testgenie.uiTest.pages.WelcomeFrame
 import nl.tudelft.ewi.se.ciselab.testgenie.uiTest.utils.RemoteRobotExtension
 import org.junit.jupiter.api.AfterAll
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.MethodOrderer
 import org.junit.jupiter.api.Order
@@ -21,6 +18,11 @@ import java.time.Duration
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ExtendWith(RemoteRobotExtension::class)
 class QuickAccessParametersTest {
+
+    /**
+     * Opens an untitled project from the IntelliJ welcome screen.
+     * Then opens the TestGenie sidebar on the right.
+     */
     @BeforeAll
     fun setUpAll(remoteRobot: RemoteRobot): Unit = with(remoteRobot) {
         find(WelcomeFrame::class.java, timeout = Duration.ofSeconds(15)).apply {
@@ -28,23 +30,23 @@ class QuickAccessParametersTest {
         }
 
         find(IdeaFrame::class.java, timeout = Duration.ofSeconds(15)).apply {
-            waitForBackgroundTasks()
-            openToolWindow()
+            clickOnToolWindow()
         }
     }
 
     @Order(1)
     @Test
-    fun checkTestGenieToolWindowPanel(remoteRobot: RemoteRobot) = with(remoteRobot) {
-        val quickAccessParametersFrame = find(QuickAccessParametersFrame::class.java, timeout = Duration.ofSeconds(15))
-        quickAccessParametersFrame.openQuickAccessParameters()
-        Assertions.assertTrue(quickAccessParametersFrame.titleGetFindJLabel.isShowing)
-    }
+    fun checkTestGenieToolWindowPanel(remoteRobot: RemoteRobot) = with(remoteRobot) {}
 
+    /**
+     * First closes the TestGenie sidebar by clicking on the stripe button again.
+     * Secondly, closes the project itself and returns the sandbox to IntelliJ welcome state.
+     */
     @AfterAll
     fun closeAll(remoteRobot: RemoteRobot): Unit = with(remoteRobot) {
-        find(SettingsFrame::class.java, timeout = Duration.ofSeconds(15)).apply {
-            closeSettings()
+
+        find(IdeaFrame::class.java, timeout = Duration.ofSeconds(15)).apply {
+            clickOnToolWindow()
         }
 
         find(IdeaFrame::class.java, timeout = Duration.ofSeconds(15)).apply {
