940406f7c164d10ed47960b3d965f854f035bf30
Martin Mladenov <m.mladenov@student.tudelft.nl>
2022-05-17 16:02:06 +0200

    Add event handler to the apply button

--- src/main/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/services/TestCaseDisplayService.kt
+++ src/main/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/services/TestCaseDisplayService.kt
@@ -2,8 +2,7 @@ package nl.tudelft.ewi.se.ciselab.testgenie.services
 
 import com.intellij.ide.highlighter.JavaFileType
 import com.intellij.openapi.project.Project
-import com.intellij.psi.JavaCodeFragmentFactory
-import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.*
 import com.intellij.ui.EditorTextField
 import com.intellij.ui.components.JBScrollPane
 import org.evosuite.utils.CompactReport
@@ -28,6 +27,7 @@ class TestCaseDisplayService(private val project: Project) {
     init {
         allTestCasePanel.layout = BoxLayout(allTestCasePanel, BoxLayout.Y_AXIS)
         mainPanel.layout = BorderLayout()
+        applyButton.addActionListener { applyTests() }
         mainPanel.add(applyButton, BorderLayout.SOUTH)
         mainPanel.add(scrollPane, BorderLayout.CENTER)
     }
@@ -81,5 +81,37 @@ class TestCaseDisplayService(private val project: Project) {
             Thread.sleep(10000)
             editor.background = backgroundDefault
         }.start()
+    }
+
+    private fun applyTests() {
+        val selectedTestCases = testCasePanels.filter { (it.value.getComponent(0) as JCheckBox).isSelected }
+            .map { it.key }
+
+
+        val testCaseComponents = selectedTestCases.map {
+            testCasePanels[it]!!.getComponent(1) as EditorTextField
+        }.map {
+//            val code = it.document.text
+//            JavaCodeFragmentFactory.getInstance(project)
+//                .createExpressionCodeFragment(code, null, null, true)
+            it.document.text
+        }
+
+        println(testCaseComponents)
+
+//        val testSuiteFile = PsiFileFactory.getInstance(project)
+//            .createFileFromText("test_suite.java", JavaFileType.INSTANCE, testSuiteClass) as PsiJavaFile
+//        val testSuiteFileDirectory = testSuiteFile.containingDirectory
+//        val testSuiteFileDirectoryVirtualFile = testSuiteFileDirectory.virtualFile
+//        val testSuiteFileDirectoryPsiDirectory =
+//            PsiManager.getInstance(project).findDirectory(testSuiteFileDirectoryVirtualFile)!!
+//        val testSuiteFilePsiDirectory = testSuiteFileDirectoryPsiDirectory.findSubdirectory("test")!!
+//        val testSuiteFilePsiFile = testSuiteFilePsiDirectory.findFile("TestSuite.java")!!
+//        val testSuiteFilePsiFileVirtualFile = testSuiteFilePsiFile.virtualFile
+//        val testSuiteFilePsiFilePsiFile = PsiManager.getInstance(project).findFile(testSuiteFilePsiFileVirtualFile)!!
+//        val testSuiteFilePsiFilePsiDirectory = testSuiteFilePsiFilePsiFile.parent!!
+//        val testSuiteFilePsiFilePsiFileDirectoryVirtualFile = testSuiteFilePsiFilePsiDirectory.virtualFile
+//        val testSuiteFilePsiFilePsiFileDirectoryPsiDirectory =
+//            PsiManager.getInstance(project).findDirectory(testSuiteFilePsiFilePsiDirectory.virtualFile)!!
     }
 }
