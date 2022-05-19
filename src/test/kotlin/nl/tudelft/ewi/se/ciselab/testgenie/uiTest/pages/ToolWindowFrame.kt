5d71cb702e9f1607bfedd21f6b8aff3d47e7bede
Jegor Zelenjak <J.Zelenjak@student.tudelft.nl>
2022-05-20 01:44:07 +0200

    Add some default tooltips and a test for them

--- src/test/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/uiTest/pages/ToolWindowFrame.kt
+++ src/test/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/uiTest/pages/ToolWindowFrame.kt
@@ -25,52 +25,130 @@ class ToolWindowFrame(remoteRobot: RemoteRobot, remoteComponent: RemoteComponent
         get() = actionLink(byXpath("//div[@class='DumbUnawareHider']"))
 
     // The title "Quick Access Parameters"
-    val title
+    private val title
         get() = jLabel(byXpath("//div[@javaclass='javax.swing.JLabel']"))
+
+    /**
+     * SEARCH BUDGET SECTION
+     */
 
     // The search budget separator line
     val searchBudgetSeparator
         get() = jLabel(byXpath("//div[@accessiblename='Search budget' and @class='JLabel' and @text='Search budget']"))
 
+    /**
+     * Search budget type
+     */
+
     // The search budget type text
     val searchBudgetTypeLabel
         get() = jLabel(byXpath("//div[@text='Search budget type']"))
 
+    // The text with the default value for the search budget type
+    val searchBudgetTypeDefaultTooltip
+        get() = jLabel(byXpath("//div[@text='Default: Max time']"))
+
+    /**
+     * Search budget (value)
+     */
     // The search budget value text
     val searchBudgetValueLabel
         get() = jLabel(byXpath("//div[@accessiblename='Search budget' and @class='JBLabel' and @text='Search budget']"))
+
+    // The text with the default value for the search budget value TODO: find it
+//    val searchBudgetValueDefaultTooltip
+//        get() = jLabel(byXpath(""))
+
+    /**
+     * TIMEOUTS SECTION
+     */
 
     // The timeouts' separator line
     val timeoutsSeparator
         get() = jLabel(byXpath("//div[@text='Timeouts']"))
 
+    /**
+     * Initialization timeout
+     */
+
     // The initialization timeout text
     val initializationTimeoutLabel
         get() = jLabel(byXpath("//div[@text='Initialization timeout']"))
+
+    // The text with the default value for the initialization timeout
+    val initializationTimeoutDefaultTooltip
+        get() = jLabel(byXpath("//div[@text='Default: 120 seconds']"))
+
+    /**
+     * Minimization timeout
+     */
 
     // The minimisation timeout text
     val minimisationTimeoutLabel
         get() = jLabel(byXpath("//div[@text='Minimisation timeout']"))
 
+    // The text with the default value for the minimisation timeout TODO: find it
+//    val minimisationTimeoutDefaultTooltip
+//        get() = jLabel(byXpath(""))
+
+    /**
+     * Assertion timeout
+     */
+
     // The assertion timeout text
     val assertionTimeoutLabel
         get() = jLabel(byXpath("//div[@text='Assertion timeout']"))
+
+    // The text with the default value for the assertion timeout TODO: find it
+//    val assertionTimeoutDefaultTooltip
+//        get() = jLabel(byXpath(""))
+
+    /**
+     * JUnit check timeout
+     */
 
     // The JUnit check timeout text
     val jUnitCheckTimeoutLabel
         get() = jLabel(byXpath("//div[@text='JUnit check timeout']"))
 
+    // The text with the default value for the JUnit check timeout TODO: find it
+//    val jUnitCheckTimeoutDefaultTooltip
+//        get() = jLabel(byXpath(""))
+
+    /**
+     * GENETIC ALGORITHM SECTION
+     */
     // The genetic algorithm separator line
     val geneticAlgorithmSeparator
         get() = jLabel(byXpath("//div[@text='Genetic Algorithm']"))
+
+    /**
+     * Population limit
+     */
 
     // The population limit text
     val populationLimitLabel
         get() = jLabel(byXpath("//div[@text='Population limit']"))
 
+    // The text with the default value for the population limit TODO: find it
+    val populationLimitDefaultTooltip
+        get() = jLabel(byXpath("//div[@text='Default: Individuals']"))
+
+    /**
+     * Population value
+     */
+
     // The population value text
     val populationValueLabel
         get() = jLabel(byXpath("//div[@text='Population']"))
+
+    // The text with the default value for the population value TODO: find it
+//    val populationValueDefaultTooltip
+//        get() = jLabel(byXpath(""))
+
+    /**
+     * ACTION LINK AND BUTTONS
+     */
 
     // The action link text
     val advancedSettingsButton
@@ -123,5 +201,19 @@ class ToolWindowFrame(remoteRobot: RemoteRobot, remoteComponent: RemoteComponent
      */
     fun getButtons(): List<JButtonFixture> {
         return listOf(saveButton, resetButton)
+    }
+
+    /**
+     * Gets the tooltips with default values of the parameters.
+     *
+     * @return the list of "default tooltips"
+     */
+    fun getDefaultTooltips(): List<JLabelFixture> {
+        // TODO: pass others once they are found
+        return listOf(
+            searchBudgetTypeDefaultTooltip,
+            initializationTimeoutDefaultTooltip,
+            populationLimitDefaultTooltip
+        )
     }
 }
