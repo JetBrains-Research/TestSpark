d7c5be7eb48e6e2dccfd05095decbbbcdde4645c
jzelenjak <J.Zelenjak@student.tudelft.nl>
2022-05-19 15:48:05 +0200

    Add KDoc for bundle objects

--- src/main/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/TestGenieDefaultsBundle.kt
+++ src/main/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/TestGenieDefaultsBundle.kt
@@ -6,6 +6,9 @@ import org.jetbrains.annotations.PropertyKey
 
 const val DEFAULTS_BUNDLE = "defaults.TestGenie"
 
+/**
+ * Loads the default values from `defaults.TestGenie` file in the `recourses` directory.
+ */
 object TestGenieDefaultsBundle : DynamicBundle(DEFAULTS_BUNDLE) {
     @Nls
     fun defaultValue(@PropertyKey(resourceBundle = DEFAULTS_BUNDLE) key: String): String = getMessage(key)
