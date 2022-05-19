95382245f33ab939d35b2277b0ad535ec416167c
jzelenjak <J.Zelenjak@student.tudelft.nl>
2022-05-19 15:44:17 +0200

    Add TestGenieDefaultsBundle to read the default values of quick access parameters

---
+++ src/main/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/TestGenieDefaultsBundle.kt
@@ -0,0 +1,12 @@
+package nl.tudelft.ewi.se.ciselab.testgenie
+
+import com.intellij.DynamicBundle
+import org.jetbrains.annotations.Nls
+import org.jetbrains.annotations.PropertyKey
+
+const val DEFAULTS_BUNDLE = "defaults.TestGenie"
+
+object TestGenieDefaultsBundle : DynamicBundle(DEFAULTS_BUNDLE) {
+    @Nls
+    fun defaultValue(@PropertyKey(resourceBundle = DEFAULTS_BUNDLE) key: String): String = getMessage(key)
+}
