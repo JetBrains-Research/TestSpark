2b096df89fd0d1a5e4afeb4f740b5c5c292dc71b
Jegor Zelenjak <J.Zelenjak@student.tudelft.nl>
2022-05-20 12:17:57 +0200

    Add JSpinner fixture into CustomFixtures.kt. Remove unused fixtures from CustomFixtures.kt

--- src/test/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/uiTest/customfixtures/CustomFixtures.kt
+++ src/test/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/uiTest/customfixtures/CustomFixtures.kt
@@ -6,39 +6,17 @@ import com.intellij.remoterobot.fixtures.ComponentFixture
 import com.intellij.remoterobot.fixtures.ContainerFixture
 import com.intellij.remoterobot.fixtures.FixtureName
 import com.intellij.remoterobot.search.locators.Locator
-import com.intellij.remoterobot.search.locators.byXpath
-import com.intellij.remoterobot.utils.waitFor
 import java.time.Duration
-
-/**
- * Function, which looks for the TabLabel.
- */
-fun ContainerFixture.openContentTabLabel(name: String, seconds: Long): ContentTabLabel {
-    val xpath = byXpath("$name", "//div[@accessiblename='$name' and @class='ContentTabLabel']")
-    waitFor(Duration.ofSeconds(seconds)) {
-        findAll<ContentTabLabel>(xpath).isNotEmpty()
-    }
-    return findAll<ContentTabLabel>(xpath).first()
-}
-
-/**
- * This class represents the TabLAbel.
- */
-@FixtureName("ContentTabLabel")
-class ContentTabLabel(remoteRobot: RemoteRobot, remoteComponent: RemoteComponent) : ComponentFixture(remoteRobot, remoteComponent)
-
-/**
- * Function, which looks for the StripeButton.
- */
-fun ContainerFixture.stripeButton(locator: Locator): StripeButtonFixture {
-    return find(locator, Duration.ofSeconds(60))
-}
 
 /**
  * This class represents the StripeButton.
  */
-@FixtureName("StripeButton")
-open class StripeButtonFixture(
+@FixtureName("JSpinner")
+open class JSpinnerFixture(
     remoteRobot: RemoteRobot,
     remoteComponent: RemoteComponent
 ) : ComponentFixture(remoteRobot, remoteComponent)
+
+fun ContainerFixture.jSpinner(locator: Locator): JSpinnerFixture {
+    return find(locator, Duration.ofSeconds(60))
+}
