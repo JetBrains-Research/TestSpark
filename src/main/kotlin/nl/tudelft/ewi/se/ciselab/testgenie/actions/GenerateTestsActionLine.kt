900d387a7c2d1863979eaab4dc0c0a8c3cc6174d
Sergey Datskiv <S.Datskiv@student.tudelft.nl>
2022-05-23 18:21:49 +0200

    Create GenerateTestActionLine class
    Create GenerateTestActionLine class and add that in Plugin.xml.
    
    Co-Authored-By: Jegor Zelenjak <45541174+jzelenjak@users.noreply.github.com>

---
+++ src/main/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/actions/GenerateTestsActionLine.kt
@@ -0,0 +1,9 @@
+package nl.tudelft.ewi.se.ciselab.testgenie.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class GenerateTestsActionLine : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+    }
+}
