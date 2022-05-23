b416c65ba0ad1bc44cb8df0b8f904725c6125c29
Sergey Datskiv <S.Datskiv@student.tudelft.nl>
2022-05-23 19:53:01 +0200

    Identify line number
    Identify line number by editing GenerateTestsActionLine and GenerateTestsUtils.
    
    Co-Authored-By: Jegor Zelenjak <45541174+jzelenjak@users.noreply.github.com>

--- src/main/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/actions/GenerateTestsActionLine.kt
+++ src/main/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/actions/GenerateTestsActionLine.kt
@@ -2,8 +2,40 @@ package nl.tudelft.ewi.se.ciselab.testgenie.actions
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.editor.Caret
+import com.intellij.openapi.editor.Document
+import com.intellij.openapi.ui.Messages
+import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.PsiFile
 
 class GenerateTestsActionLine : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
+    }
+
+    /**
+     * Makes the action visible only if a line has been selected.
+     * It also updates the action name depending on which line has been selected.
+     *
+     * @param e an action event that contains useful information
+     */
+    override fun update(e: AnActionEvent) {
+        e.presentation.isEnabledAndVisible = false
+
+        e.project ?: return
+
+        val caret: Caret = e.dataContext.getData(CommonDataKeys.CARET)?.caretModel?.primaryCaret ?: return
+        val psiFile: PsiFile = e.dataContext.getData(CommonDataKeys.PSI_FILE) ?: return
+
+        getSurroundingMethod(psiFile, caret) ?: return
+        val doc: Document = PsiDocumentManager.getInstance(e.project!!).getDocument(psiFile) ?: return
+        val line: Int? = getSurroundingLine(doc, caret)
+        println(line)
+        // TODO Ignore declarations
+        return
+        Messages.showInfoMessage("Selected line: $line\nSelected text:\n$", "la")
+
+        e.presentation.isEnabledAndVisible = true
+//        e.presentation.text = "Generate Tests For ${getMethodDisplayName(psiMethod)}"
     }
 }
