8da1e4f8bc33f3d6ea946ec2eacab68e9c13cf61
Sergey Datskiv <S.Datskiv@student.tudelft.nl>
2022-05-24 19:03:56 +0200

    Update actionPerformed() method for line and added KDoc
    Co-Authored-By: Jegor Zelenjak <45541174+jzelenjak@users.noreply.github.com>

--- src/main/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/actions/GenerateTestsActionLine.kt
+++ src/main/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/actions/GenerateTestsActionLine.kt
@@ -3,11 +3,46 @@ package nl.tudelft.ewi.se.ciselab.testgenie.actions
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.Caret
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ProjectRootManager
+import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiFile
 
+/**
+ * This class contains all the logic related to generating tests for a line.
+ * No actual generation happens in this class, rather it is responsible for displaying the action option to the user when it is available,
+ *   getting the information about the selected class and passing it to (EvoSuite) Runner.
+ */
 class GenerateTestsActionLine : AnAction() {
+    private val log = Logger.getInstance(this.javaClass)
+
+    /**
+     * Performs test generation for a line when the action is invoked.
+     *
+     * @param e an action event that contains useful information
+     */
     override fun actionPerformed(e: AnActionEvent) {
+        val project: Project = e.project ?: return
+
+        val psiFile: PsiFile = e.dataContext.getData(CommonDataKeys.PSI_FILE) ?: return
+        val caret: Caret = e.dataContext.getData(CommonDataKeys.CARET)?.caretModel?.primaryCaret ?: return
+
+        val psiClass: PsiClass = getSurroundingClass(psiFile, caret) ?: return
+        val classFQN = psiClass.qualifiedName ?: return
+
+        val line: Int = getSurroundingLine(psiFile, caret) ?: return
+
+        val projectPath: String = ProjectRootManager.getInstance(project).contentRoots.first().path
+        val projectClassPath = "$projectPath/target/classes/"
+
+        log.info("Generating tests for project $projectPath with classpath $projectClassPath")
+
+        log.info("Selected class is $classFQN")
+        log.info("Selected line is $line")
+
+        // Runner(project, projectPath, projectClassPath, classFQN, fileName, modificationStamp).forClass().runEvoSuite()
     }
 
     /**
