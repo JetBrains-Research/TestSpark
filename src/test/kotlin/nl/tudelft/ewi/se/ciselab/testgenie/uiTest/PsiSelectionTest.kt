c66b3f83c317d529e73bbe7681d8ab81753331d5
Jegor Zelenjak <J.Zelenjak@student.tudelft.nl>
2022-05-25 19:21:45 +0200

    Add new variables to IdeaFrame.kt and PsiSelectionTest.kt

---
+++ src/test/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/uiTest/PsiSelectionTest.kt
@@ -0,0 +1,56 @@
+package nl.tudelft.ewi.se.ciselab.testgenie.uiTest
+
+import com.intellij.remoterobot.RemoteRobot
+import nl.tudelft.ewi.se.ciselab.testgenie.uiTest.pages.IdeaFrame
+import nl.tudelft.ewi.se.ciselab.testgenie.uiTest.pages.WelcomeFrame
+import nl.tudelft.ewi.se.ciselab.testgenie.uiTest.utils.RemoteRobotExtension
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.TestMethodOrder
+import org.junit.jupiter.api.extension.ExtendWith
+import java.time.Duration
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(RemoteRobotExtension::class)
+class PsiSelectionTest {
+    private lateinit var remoteRobot: RemoteRobot
+
+    /**
+     * Opens an untitled project from the IntelliJ welcome screen.
+     */
+    @BeforeAll
+    fun setUpAll(remoteRobot: RemoteRobot): Unit = with(remoteRobot) {
+        // Open an 'untitled' projectLabel
+        find(WelcomeFrame::class.java, timeout = Duration.ofSeconds(15)).apply {
+            open("pizzeria")
+        }
+
+        find(IdeaFrame::class.java, timeout = Duration.ofSeconds(15)).apply {
+            openProjectFromProjectTree()
+        }
+    }
+
+    @BeforeEach
+    fun setUp(_remoteRobot: RemoteRobot): Unit = with(_remoteRobot) {
+        remoteRobot = _remoteRobot
+    }
+
+    @Test
+    fun suspiciousTest() {
+        assertThat("Marti").isEqualTo("Marti")
+    }
+
+    @AfterAll
+    fun closeAll(remoteRobot: RemoteRobot): Unit = with(remoteRobot) {
+        // Close the project
+        find(IdeaFrame::class.java, timeout = Duration.ofSeconds(15)).apply {
+            closeProject()
+        }
+    }
+}
