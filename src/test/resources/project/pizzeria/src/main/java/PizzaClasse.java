879ebb7dcf09a727c936a30b3986b0296b695062
Jegor Zelenjak <J.Zelenjak@student.tudelft.nl>
2022-05-26 15:42:08 +0200

    Make robot enter full screen

--- src/test/resources/project/pizzeria/src/main/java/PizzaClasse.java
+++ src/test/resources/project/pizzeria/src/main/java/PizzaClasse.java
@@ -6,22 +6,15 @@ public class PizzaClasse {
     public static void main(String[] args) {
         pizzaMetodo();
         pizzaMetodo(256);
-
         PizzaServizio pizzaServizio = new PizzaServizio() {
             @Override
-            public PizzaAstratta fareLaPizza() {
-                return new BuonaPizza();
-            }
+            public PizzaAstratta fareLaPizza() { return new BuonaPizza(); }
         };
-
         pizzaServizio.saluto();
         PizzaAstratta pizza = pizzaServizio.fareLaPizza();
-
     }
 
-    public static void pizzaMetodo() {
-        System.out.println("Io sono una buona pizza! Mi costa " + prezzoBase + " euro.");
-    }
+    public static void pizzaMetodo() { System.out.println("Io sono una buona pizza! Mi costa " + prezzoBase + " euro."); }
 
     public static void pizzaMetodo(int prezzo) {
         System.out.println("Io sono una buona pizza! Mi costa " + prezzo + " euro.");
@@ -29,10 +22,7 @@ public class PizzaClasse {
 }
 
 interface PizzaServizio {
-    default void saluto() {
-        System.out.println("Viva la pizza Italiana");
-    }
-
+    default void saluto() { System.out.println("Viva la pizza Italiana"); }
     PizzaAstratta fareLaPizza();
 }
 
@@ -40,15 +30,11 @@ abstract class PizzaAstratta {
     public void saluto() {
         System.out.println("Io sono una pizza astratta. Non posso essere istanziato");
     }
-
     public abstract List<String> selencaGliIngredienti();
 }
 
 class BuonaPizza extends PizzaAstratta {
-
     @Override
-    public List<String> selencaGliIngredienti() {
-        return List.of("Salame", "Mozzarella", "Salsa di pomodoro");
-    }
+    public List<String> selencaGliIngredienti() { return List.of("Salame", "Mozzarella", "Salsa di pomodoro"); }
 }
 
