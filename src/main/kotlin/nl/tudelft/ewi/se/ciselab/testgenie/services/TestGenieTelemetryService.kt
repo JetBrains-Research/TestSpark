44494f2a29ba8db118f77ef32cdb26f591cfe785
Martin Mladenov <m.mladenov@student.tudelft.nl>
2022-05-27 15:40:56 +0200

    Improve thread safety

--- src/main/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/services/TestGenieTelemetryService.kt
+++ src/main/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/services/TestGenieTelemetryService.kt
@@ -5,18 +5,23 @@ import com.intellij.openapi.diagnostic.Logger
 
 class TestGenieTelemetryService() {
     private val modifiedTestCases = mutableListOf<ModifiedTestCase>()
+    private val modifiedTestCasesLock = Object()
 
     private val log: Logger = Logger.getInstance(this.javaClass)
 
     fun scheduleTestCasesForTelemetry(testCases: List<ModifiedTestCase>) {
-        modifiedTestCases.addAll(testCases)
+        synchronized(modifiedTestCasesLock) {
+            modifiedTestCases.addAll(testCases)
+        }
     }
 
     fun uploadScheduledTestCases() {
         val testCasesToUpload = mutableListOf<ModifiedTestCase>()
 
-        testCasesToUpload.addAll(modifiedTestCases)
-        modifiedTestCases.clear()
+        synchronized(modifiedTestCasesLock) {
+            testCasesToUpload.addAll(modifiedTestCases)
+            modifiedTestCases.clear()
+        }
 
         val gson = Gson()
         val json = gson.toJson(testCasesToUpload)
