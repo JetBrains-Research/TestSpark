db30d658596c29b978c0e87e1d5c5188f16d62e1
Martin Mladenov <m.mladenov@student.tudelft.nl>
2022-05-27 15:39:12 +0200

    Create method to schedule test cases for telemetry

--- src/main/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/services/TestGenieTelemetryService.kt
+++ src/main/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/services/TestGenieTelemetryService.kt
@@ -1,8 +1,11 @@
 package nl.tudelft.ewi.se.ciselab.testgenie.services
 
-
 class TestGenieTelemetryService() {
     private val modifiedTestCases = mutableListOf<ModifiedTestCase>()
+
+    fun scheduleTestCasesForTelemetry(testCases: List<ModifiedTestCase>) {
+        modifiedTestCases.addAll(testCases)
+    }
 
     class ModifiedTestCase(val original: String, val modified: String)
 }
