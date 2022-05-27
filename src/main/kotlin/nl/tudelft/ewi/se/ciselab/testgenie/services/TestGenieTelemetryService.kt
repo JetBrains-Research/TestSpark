f6cedf04f29ab98888876587949a1dc78d490b76
Martin Mladenov <m.mladenov@student.tudelft.nl>
2022-05-27 15:38:36 +0200

    Create modified test case

--- src/main/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/services/TestGenieTelemetryService.kt
+++ src/main/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/services/TestGenieTelemetryService.kt
@@ -1,4 +1,8 @@
 package nl.tudelft.ewi.se.ciselab.testgenie.services
 
+
 class TestGenieTelemetryService() {
+    private val modifiedTestCases = mutableListOf<ModifiedTestCase>()
+
+    class ModifiedTestCase(val original: String, val modified: String)
 }
