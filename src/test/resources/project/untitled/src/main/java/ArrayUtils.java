00ee06b2f66648d24cf58fa79ca507b14c7d85e7
Jegor Zelenjak <J.Zelenjak@student.tudelft.nl>
2022-05-27 18:03:58 +0200

    Remove blank lines from ArrayUtils.java

--- src/test/resources/project/untitled/src/main/java/ArrayUtils.java
+++ src/test/resources/project/untitled/src/main/java/ArrayUtils.java
@@ -1,11 +1,5 @@
 /**
- * (From Apache Commons Lang: https://github.com/apache/commons-lang/blob/d2687419c6973572d1621afc1b8546f5262769c3/src/main/java/org/apache/commons/lang3/
- *
- *
- *
- *
- *
- * ArrayUtils.java#L2325)
+ * (From Apache Commons Lang: https://github.com/apache/commons-lang/blob/d2687419c6973572d1621afc1b8546f5262769c3/src/main/java/org/apache/commons/lang3/ArrayUtils.java#L2325)
  */
 class ArrayUtils {
 
