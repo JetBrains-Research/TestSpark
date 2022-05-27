e73a0447081ddd83a211e97265e44edd95588ebc
Martin Mladenov <m.mladenov@student.tudelft.nl>
2022-05-27 15:39:51 +0200

    Create method to upload scheduled test cases

--- src/main/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/services/TestGenieTelemetryService.kt
+++ src/main/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/services/TestGenieTelemetryService.kt
@@ -1,10 +1,27 @@
 package nl.tudelft.ewi.se.ciselab.testgenie.services
 
+import com.google.gson.Gson
+import com.intellij.openapi.diagnostic.Logger
+
 class TestGenieTelemetryService() {
     private val modifiedTestCases = mutableListOf<ModifiedTestCase>()
 
+    private val log: Logger = Logger.getInstance(this.javaClass)
+
     fun scheduleTestCasesForTelemetry(testCases: List<ModifiedTestCase>) {
         modifiedTestCases.addAll(testCases)
+    }
+
+    fun uploadScheduledTestCases() {
+        val testCasesToUpload = mutableListOf<ModifiedTestCase>()
+
+        testCasesToUpload.addAll(modifiedTestCases)
+        modifiedTestCases.clear()
+
+        val gson = Gson()
+        val json = gson.toJson(testCasesToUpload)
+
+        // TODO: Actually upload test cases to server
     }
 
     class ModifiedTestCase(val original: String, val modified: String)
