62c47999e692d75ac08c2cfc0edd02c5b359d78c
Bolek Khodakov <b.a.khodakov@student.tudelft.nl>
2022-05-27 14:31:40 +0200

    Change highlighter in "update" to additional syntax too
    Same changes as previously, just on another method

--- src/main/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/services/CoverageVisualisationService.kt
+++ src/main/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/services/CoverageVisualisationService.kt
@@ -80,7 +80,7 @@ class CoverageVisualisationService(private val project: Project) {
                 val line = i - 1
                 val textAttributesKey = TextAttributesKey.createTextAttributesKey("custom")
                 textAttributesKey.defaultAttributes.backgroundColor = colorForLines
-                val hl = editor.markupModel.addLineHighlighter(textAttributesKey, line, HighlighterLayer.LAST)
+                val hl = editor.markupModel.addLineHighlighter(textAttributesKey, line, HighlighterLayer.ADDITIONAL_SYNTAX)
 
                 hl.lineMarkerRenderer = CoverageRenderer(
                     color,
