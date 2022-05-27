f391ae93fae3ea100aa3aea4db96c0481798bf76
Sergey Datskiv <S.Datskiv@student.tudelft.nl>
2022-05-27 12:27:15 +0200

    Reformat PizzaClasse to fit on a screen
    Reformat PizzaClasse to fit the entire class on the screen (for localhost readability) and add comments for PSI UI testing purposes.

--- src/test/resources/project/pizzeria/src/main/java/PizzaClasse.java
+++ src/test/resources/project/pizzeria/src/main/java/PizzaClasse.java
@@ -1,8 +1,6 @@
 import java.util.List;
-
 public class PizzaClasse {
     private static final int prezzoBase = 42;
-
     public static void main(String[] args) {
         pizzaMetodo();
         pizzaMetodo(256);
@@ -13,28 +11,21 @@ public class PizzaClasse {
         pizzaServizio.saluto();
         PizzaAstratta pizza = pizzaServizio.fareLaPizza();
     }
-
     public static void pizzaMetodo() { System.out.println("Io sono una buona pizza! Mi costa " + prezzoBase + " euro."); }
+    // sono un commento
 
-    public static void pizzaMetodo(int prezzo) {
-        System.out.println("Io sono una buona pizza! Mi costa " + prezzo + " euro.");
-    }
+    public static void pizzaMetodo(int prezzo) { System.out.println("Io sono anche una buona pizza! Mi costa " + prezzo + " euro."); }
 }
-
 interface PizzaServizio {
     default void saluto() { System.out.println("Viva la pizza Italiana"); }
     PizzaAstratta fareLaPizza();
 }
-
 abstract class PizzaAstratta {
-    public void saluto() {
-        System.out.println("Io sono una pizza astratta. Non posso essere istanziato");
-    }
+    public void saluto() { System.out.println("Io sono una pizza astratta. Non posso essere istanziato"); }
     public abstract List<String> selencaGliIngredienti();
 }
-
 class BuonaPizza extends PizzaAstratta {
-    @Override
+    @Override // Overriden function
     public List<String> selencaGliIngredienti() { return List.of("Salame", "Mozzarella", "Salsa di pomodoro"); }
 }
 
