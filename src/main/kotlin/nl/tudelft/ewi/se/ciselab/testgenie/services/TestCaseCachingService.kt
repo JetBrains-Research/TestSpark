8357061365c756384e0da1dbdca062c488a6c5ba
Martin Mladenov <m.mladenov@student.tudelft.nl>
2022-05-30 23:43:58 +0200

    Create TestCaseCachingService

---
+++ src/main/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/services/TestCaseCachingService.kt
@@ -0,0 +1,9 @@
+package nl.tudelft.ewi.se.ciselab.testgenie.services
+
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.project.Project
+
+class TestCaseCachingService(private val project: Project) {
+    private val log: Logger = Logger.getInstance(this.javaClass)
+
+}
