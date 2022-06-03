51096ed6648078d6c49202f7a38d85344263156f
Jegor Zelenjak <J.Zelenjak@student.tudelft.nl>
2022-06-03 17:04:24 +0200

    Add a larger timeout for project TestGenieSettingsState.kt
    Co-Authored-By: Sergey Datskiv <S.Datskiv@student.tudelft.nl>

--- src/main/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/settings/TestGenieSettingsState.kt
+++ src/main/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/settings/TestGenieSettingsState.kt
@@ -63,7 +63,7 @@ data class TestGenieSettingsState(
             val suffix = TestGenieDefaultsBundle.defaultValue("telemetryDirectory")
             val backupDefaultTelemetryPath: String = System.getProperty("user.home").plus(suffix)
 
-            val dataContext: DataContext = DataManager.getInstance().dataContextFromFocusAsync.blockingGet(5000)
+            val dataContext: DataContext = DataManager.getInstance().dataContextFromFocusAsync.blockingGet(15000)
                 ?: return backupDefaultTelemetryPath
             val projectBasePath: String = CommonDataKeys.PROJECT.getData(dataContext)?.basePath
                 ?: return backupDefaultTelemetryPath
