103c5ac13d5038a9a8c57259e5d29bb40e0351ea
Sergey Datskiv <S.Datskiv@student.tudelft.nl>
2022-06-10 12:59:48 +0200

    Add tests for  deselect and select all buttons
    Add tests that apply all tests or no tests to the specified test file.

--- src/test/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/uiTest/GeneratedTestsToolWindowTest.kt
+++ src/test/kotlin/nl/tudelft/ewi/se/ciselab/testgenie/uiTest/GeneratedTestsToolWindowTest.kt
@@ -2,6 +2,9 @@ package nl.tudelft.ewi.se.ciselab.testgenie.uiTest
 
 import com.automation.remarks.junit5.Video
 import com.intellij.remoterobot.RemoteRobot
+import com.intellij.remoterobot.fixtures.EditorFixture
+import com.intellij.remoterobot.search.locators.byXpath
+import com.intellij.remoterobot.utils.keyboard
 import nl.tudelft.ewi.se.ciselab.testgenie.uiTest.pages.IdeaFrame
 import nl.tudelft.ewi.se.ciselab.testgenie.uiTest.pages.WelcomeFrame
 import nl.tudelft.ewi.se.ciselab.testgenie.uiTest.utils.RemoteRobotExtension
@@ -14,12 +17,18 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.TestMethodOrder
 import org.junit.jupiter.api.extension.ExtendWith
+import java.awt.event.KeyEvent
 import java.time.Duration
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ExtendWith(RemoteRobotExtension::class)
 class GeneratedTestsToolWindowTest {
+
+    private val emptyClass = """public class ArrayUtilsTest {
+}
+"""
+    private lateinit var editor: EditorFixture
 
     @BeforeAll
     fun setUpAll(remoteRobot: RemoteRobot): Unit = with(remoteRobot) {
@@ -59,6 +68,50 @@ class GeneratedTestsToolWindowTest {
             Assertions.assertThat(selectAll.hasText("Select All")).isTrue
             Assertions.assertThat(deselectAll.hasText("Deselect All")).isTrue
             Assertions.assertThat(applyToTestSuite.hasText("Apply to test suite")).isTrue
+        }
+    }
+
+    @Order(2)
+    @Video
+    @Test
+    fun testApplyTestsButtonDeselectAll(remoteRobot: RemoteRobot): Unit = with(remoteRobot) {
+        val ideaFrame = find(IdeaFrame::class.java, timeout = Duration.ofSeconds(15))
+        ideaFrame.apply {
+            // Deselect all and apply to testSuite.
+            deselectAllApplyTestsToTestSuite("ArrayUtilsTest")
+            // Open the correct file.
+            openProjectFile("ArrayUtilsTest", "untitled")
+            // Close tool window to decrease the number of editors.
+            clickOnToolWindow()
+            editor = find(byXpath("//div[@class='EditorComponentImpl']"))
+            // Assert that the test file only has the class signature and nothing else.
+            Assertions.assertThat(editor.text).isEqualTo(emptyClass)
+            // Close the ArrayUtilsTest file tab.
+            remoteRobot.keyboard { hotKey(KeyEvent.VK_CONTROL, KeyEvent.VK_F4) }
+            // Restore the state as before this test by opening the tool window again.
+            clickOnToolWindow()
+        }
+    }
+
+    @Order(3)
+    @Video
+    @Test
+    fun testApplyTestsButtonSelectAll(remoteRobot: RemoteRobot): Unit = with(remoteRobot) {
+        val ideaFrame = find(IdeaFrame::class.java, timeout = Duration.ofSeconds(15))
+        ideaFrame.apply {
+            // Select all and apply to testSuite.
+            selectAllApplyTestsToTestSuite("ArrayUtilsTest")
+            // Open the correct file.
+            openProjectFile("ArrayUtilsTest", "untitled")
+            // Close tool window to decrease the number of editors.
+            clickOnToolWindow()
+            editor = find(byXpath("//div[@class='EditorComponentImpl']"))
+            // Assert that tests were appended by checking the length of empty class and the new class.
+            Assertions.assertThat(editor.text.length).isGreaterThan(emptyClass.length)
+            // Close the ArrayUtilsTest file tab.
+            remoteRobot.keyboard { hotKey(KeyEvent.VK_CONTROL, KeyEvent.VK_F4) }
+            // Restore the state as before this test by opening the tool window again.
+            clickOnToolWindow()
         }
     }
 
