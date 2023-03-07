2d56fa247868274f23e955205741340b108753ae
Arkadii Sapozhnikov <arkadii.sapozhnikov@jetbrains.com>
2023-03-07 17:31:06 +0100

    fix code style

--- src/main/kotlin/org/jetbrains/research/testgenie/services/TestCaseDisplayService.kt
+++ src/main/kotlin/org/jetbrains/research/testgenie/services/TestCaseDisplayService.kt
@@ -391,10 +391,10 @@ class TestCaseDisplayService(private val project: Project) {
                     appendTestsToClass(testCaseComponents, psiClass, psiFile)
                 }
             } else {
-                val psiJavaFile: PsiJavaFile =
-                    (PsiManager.getInstance(project).findFile(
-                        LocalFileSystem.getInstance().findFileByIoFile(fileChooser.selectedFile)!!
-                    ) as PsiJavaFile)
+                val psiJavaFile: PsiJavaFile = (
+                        PsiManager.getInstance(project).findFile(
+                            LocalFileSystem.getInstance().findFileByIoFile(fileChooser.selectedFile)!!
+                        ) as PsiJavaFile)
                 WriteCommandAction.runWriteCommandAction(project) {
                     appendTestsToClass(testCaseComponents, psiJavaFile.classes[0], psiJavaFile)
                 }
