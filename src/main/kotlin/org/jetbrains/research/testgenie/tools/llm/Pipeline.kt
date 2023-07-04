f4859d4d42c8dd625c0be08f18c348dbaa8ee16b
Pouria Derakhshanfar <pouria.derakhshanfar@gmail.com>
2023-07-04 11:15:13 +0200

    Move prompt generation to PromptManager

--- src/main/kotlin/org/jetbrains/research/testgenie/tools/llm/Pipeline.kt
+++ src/main/kotlin/org/jetbrains/research/testgenie/tools/llm/Pipeline.kt
@@ -16,6 +16,7 @@ import org.jetbrains.research.testgenie.editor.Workspace
 import org.jetbrains.research.testgenie.services.TestCaseDisplayService
 import org.jetbrains.research.testgenie.tools.ProjectBuilder
 import org.jetbrains.research.testgenie.tools.llm.generation.LLMProcessManager
+import org.jetbrains.research.testgenie.tools.llm.generation.PromptManager
 import java.io.File
 import java.util.*
 
@@ -46,57 +47,16 @@ class Pipeline(
     // TODO move all interactions with Workspace to Manager
     var key = Workspace.TestJobInfo(fileUrl, classFQN, modTs, testResultName, projectClassPath)
 
+    private val promptManager = PromptManager(cut, interestingPsiClasses,polymorphismRelations)
+
     private val processManager =
         LLMProcessManager(project, projectClassPath)
 
     // TODO("Removed unused input parameters. needs o be refactored after finalizing the implementation")
 
     fun forClass(): Pipeline {
-        prompt = generatePrompt()
+        prompt = promptManager.generatePrompt()
         return this
-    }
-
-    private fun generatePrompt(): String {
-        // prompt: start the request
-        var prompt =
-            "Generate unit tests in Java for ${getClassDisplayName(cut)} to achieve 100% line coverage for this class.\nDont use @Before and @After test methods.\nMake tests as atomic as possible.\nAll tests should be for JUnit 4.\nIn case of mocking, use Mockito 5. But, do not use mocking for all tests.\n"
-
-        // prompt: source code
-        prompt += "The source code of class under test is as follows:\n```\n${getClassFullText(cut)}\n```\n"
-
-        // prompt: signature of methods in the classes used by CUT
-        prompt += "Here are the method signatures of classes used by the class under test. Only use these signatures for creating objects, not your own ideas.\n"
-        for (interestingPsiClass: PsiClass in interestingPsiClasses) {
-            if (interestingPsiClass.qualifiedName!!.startsWith("java")) {
-                continue
-            }
-            val interestingPsiClassQN = interestingPsiClass.qualifiedName
-            if (interestingPsiClassQN.equals(cut.qualifiedName)) {
-                continue
-            }
-
-            prompt += "=== methods in ${getClassDisplayName(interestingPsiClass)}:\n"
-            for (currentPsiMethod in interestingPsiClass.allMethods) {
-                // Skip java methods
-                if (currentPsiMethod.containingClass!!.qualifiedName!!.startsWith("java")) {
-                    continue
-                }
-                prompt += " - ${currentPsiMethod.getSignatureString()}\n"
-            }
-            prompt += "\n\n"
-        }
-
-        // prompt: add polymorphism relations between involved classes
-        prompt += "=== polymorphism relations:\n"
-        polymorphismRelations.forEach { entry ->
-            for (currentSubClass in entry.value) {
-                prompt += "${currentSubClass.qualifiedName} is a sub-class of ${entry.key.qualifiedName}.\n"
-            }
-        }
-        // Make sure that LLM does not provide extra information other than the test file
-        prompt += "put the generated test between ```"
-
-        return prompt
     }
 
     fun runTestGeneration() {
