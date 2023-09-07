8eb2b6521d27952ccc7d60340ad9ada4d2c7cac9
Arkadii Sapozhnikov <arkadii.sapozhnikov@jetbrains.com>
2023-09-07 21:45:39 +0200

    fix imports

--- src/main/kotlin/org/jetbrains/research/testspark/services/TestCaseDisplayService.kt
+++ src/main/kotlin/org/jetbrains/research/testspark/services/TestCaseDisplayService.kt
@@ -9,9 +9,7 @@ import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diff.DiffColors
-import com.intellij.openapi.editor.Document
 import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.editor.EditorFactory
 import com.intellij.openapi.editor.event.DocumentEvent
 import com.intellij.openapi.editor.event.DocumentListener
 import com.intellij.openapi.editor.markup.HighlighterLayer
