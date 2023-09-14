c79e7815f52bc94467752b5981984d4a531fa7b8
Vladislav Artiukhov <vladislav0art.work@gmail.com>
2023-09-14 21:02:10 +0300

    Create TestsByPreferenceProvider and move TestState into separate file

---
+++ src/main/kotlin/org/jetbrains/research/testspark/data/TestState.kt
@@ -0,0 +1,10 @@
+package org.jetbrains.research.testspark.data
+
+/**
+ * Enum class which contains states of tests in terms of preference of a user.
+ */
+enum class TestState {
+    UNSPECIFIED,
+    LIKED,
+    DISLIKED
+}
