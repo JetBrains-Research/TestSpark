961730e7ffe07ad77185ab49066ace873a9155e4
Vladislav Artiukhov <vladislav0art.work@gmail.com>
2023-09-14 15:49:14 +0300

    Implement background coloring on click and custom notification message & refactor code

--- src/main/kotlin/org/jetbrains/research/testspark/services/TestPreferenceButtonsManager.kt
+++ src/main/kotlin/org/jetbrains/research/testspark/services/TestPreferenceButtonsManager.kt
@@ -3,6 +3,7 @@ package org.jetbrains.research.testspark.services
 import org.jetbrains.research.testspark.TestSparkLabelsBundle
 import org.jetbrains.research.testspark.helpers.storage.KeyValueStore
 import org.jetbrains.research.testspark.helpers.storage.KeyValueStoreFactory
+import java.awt.Color
 import java.awt.event.ActionEvent
 import java.awt.event.ActionListener
 import java.io.IOException
@@ -13,13 +14,18 @@ import javax.swing.JButton
 import javax.swing.JFrame
 import javax.swing.JOptionPane
 import javax.swing.JPanel
+import javax.swing.border.LineBorder
 
-class TestPreferenceButtonsManager (val frame: JFrame, private val workingDir: Path, private val testCode: String, private val testName: String) {
+class TestPreferenceButtonsManager (private val workingDir: Path, private val testCode: String, private val testName: String) {
     private val likeButton = JButton(TestSparkLabelsBundle.defaultValue("likeButton"))
     private val dislikeButton = JButton(TestSparkLabelsBundle.defaultValue("dislikeButton"))
+    private val defaultButtonBackgroundColor = JButton("").background
+
     private val likedTestsDir = Paths.get(workingDir.toString(), "liked-tests/")
     private val dislikedTestsDir = Paths.get(workingDir.toString(), "disliked-tests/")
     private val maxValueFileSize = 1024;
+
+    val clickNotificationFrame = JFrame("Kotlin Swing Application")
 
     public enum class TestState {
         UNSPECIFIED,
@@ -32,29 +38,51 @@ class TestPreferenceButtonsManager (val frame: JFrame, private val workingDir: P
         Files.createDirectories(likedTestsDir)
         Files.createDirectories(dislikedTestsDir)
 
+        clickNotificationFrame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+
         likeButton.addActionListener(object : ActionListener {
             override fun actionPerformed(e: ActionEvent) {
-                JOptionPane.showMessageDialog(frame, testCode, "'$testName' liked", JOptionPane.INFORMATION_MESSAGE)
-
                 val key = testName.toByteArray(Charsets.UTF_8);
                 val value = testCode.toByteArray(Charsets.UTF_8);
 
-                addIfDoesNotExist(likedTestsDir, dislikedTestsDir, key, value);
+                val entryAdded = addIfDoesNotExist(likedTestsDir, dislikedTestsDir, key, value);
 
                 println("State of test '${testName}' after liked: ${getTestState()}")
+
+                likeButton.background = Color.GREEN
+                dislikeButton.background = defaultButtonBackgroundColor
+
+                if (entryAdded) {
+                    JOptionPane.showMessageDialog(clickNotificationFrame,
+                            "Test code for the test '$testName' successfully registered as liked", "'$testName' liked", JOptionPane.INFORMATION_MESSAGE)
+                }
+                else {
+                    JOptionPane.showMessageDialog(clickNotificationFrame,
+                            "No operation performed: test code for the test '$testName' already registered as liked", "'$testName' liked", JOptionPane.INFORMATION_MESSAGE)
+                }
             }
         })
 
         dislikeButton.addActionListener(object : ActionListener {
             override fun actionPerformed(e: ActionEvent) {
-                JOptionPane.showMessageDialog(frame, testCode, "'$testName' disliked", JOptionPane.INFORMATION_MESSAGE)
-
                 val key = testName.toByteArray(Charsets.UTF_8);
                 val value = testCode.toByteArray(Charsets.UTF_8);
 
-                addIfDoesNotExist(dislikedTestsDir, likedTestsDir, key, value);
+                val entryAdded = addIfDoesNotExist(dislikedTestsDir, likedTestsDir, key, value);
 
                 println("State of test '${testName}' after disliked: ${getTestState()}")
+
+                dislikeButton.background = Color.RED
+                likeButton.background = defaultButtonBackgroundColor
+
+                if (entryAdded) {
+                    JOptionPane.showMessageDialog(clickNotificationFrame,
+                            "Test code for the test '$testName' successfully registered as disliked", "'$testName' disliked", JOptionPane.INFORMATION_MESSAGE)
+                }
+                else {
+                    JOptionPane.showMessageDialog(clickNotificationFrame,
+                            "No operation performed: test code for the test '$testName' already registered as disliked", "'$testName' disliked", JOptionPane.INFORMATION_MESSAGE)
+                }
             }
         })
     }
@@ -92,23 +120,31 @@ class TestPreferenceButtonsManager (val frame: JFrame, private val workingDir: P
         return result;
     }
 
-    private fun addIfDoesNotExist(addDir: Path, removeDir: Path, key: ByteArray, value: ByteArray) {
+    /**
+     * Adds {@code (key, value)} pair via {@link KeyValueStore} to {@code addDir} path if it has not been added yet,
+     * and removes the same pair from {@code removeDir} if it exists there.
+     *
+     * Returns {@code true} if value has been added, otherwise returns {@code false}.
+     */
+    private fun addIfDoesNotExist(addDir: Path, removeDir: Path, key: ByteArray, value: ByteArray): Boolean {
         val addStore: KeyValueStore = KeyValueStoreFactory.create(addDir, maxValueFileSize);
         val removeStore: KeyValueStore = KeyValueStoreFactory.create(removeDir, maxValueFileSize);
 
         try {
-            var written = false
+            var added = false
             var removed = false
             if (!addStore.contains(key)) {
                 addStore.upsert(key, value)
                 removed = removeStore.remove(key)
-                written = true
+                added = true
             }
 
             val result = String(addStore.loadValue(key)!!, Charsets.UTF_8)
 
-            println("For dir='${addDir.toString()}':\nwritten=$written, key='$key', value='$result'")
+            println("For dir='${addDir.toString()}':\nadded=$added, key='$key', value='$result'")
             println("For dir='${removeDir.toString()}':\nremoved=$removed, key='$key'")
+
+            return added
         }
         finally {
             removeStore.close()
