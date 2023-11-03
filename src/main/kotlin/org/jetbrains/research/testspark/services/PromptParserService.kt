7d0918808230e363f0a182bbc2a7d515987c5726
Pouria Derakhshanfar <pouria.derakhshanfar@gmail.com>
2023-11-03 18:20:11 +0100

    Add getOffsets() to PROMPT_KEYWORD

--- src/main/kotlin/org/jetbrains/research/testspark/services/PromptParserService.kt
+++ src/main/kotlin/org/jetbrains/research/testspark/services/PromptParserService.kt
@@ -9,14 +9,32 @@ import com.intellij.ui.EditorTextField
 import com.intellij.ui.JBColor
 import java.awt.Font
 
-enum class PROMPT_KEYWORD (val text: String, val description: String, val mandatory: Boolean){
-    NAME("NAME","The name of the code under test (Class name, method name, line number)",true),
-    CODE("CODE","The code under test (Class, method, or line)",true),
+enum class PROMPT_KEYWORD(val text: String, val description: String, val mandatory: Boolean) {
+    NAME("NAME", "The name of the code under test (Class name, method name, line number)", true),
+    CODE("CODE", "The code under test (Class, method, or line)", true),
     LANGUAGE("LANGUAGE", "Programming language of the project under test (only Java supported at this point)", true),
-    TESTING_PLATFORM("TESTING_PLATFORM", "testing platform used in the project (Only JUnit 4 is supported at this point)", true),
-    MOCKING_FRAMEWORK("MOCKING_FRAMEWORK", "mock framework that can be used in generated test (Only Mockito is supported at this point)", false),
+    TESTING_PLATFORM(
+        "TESTING_PLATFORM",
+        "testing platform used in the project (Only JUnit 4 is supported at this point)",
+        true
+    ),
+    MOCKING_FRAMEWORK(
+        "MOCKING_FRAMEWORK",
+        "mock framework that can be used in generated test (Only Mockito is supported at this point)",
+        false
+    ),
     METHODS("METHODS", "signature of methods used in the code under tests", false),
-    POLYMORPHISM("POLYMORPHISM", "polymorphism relations between classes involved in the code under test.", false),
+    POLYMORPHISM("POLYMORPHISM", "polymorphism relations between classes involved in the code under test.", false);
+
+    fun getOffsets(prompt: String): Pair<Int, Int>? {
+        val textToHighlight = "\$${text}"
+        if (!prompt.contains(textToHighlight))
+            return null
+
+        val startOffset = prompt.indexOf(textToHighlight)
+        val endOffset = startOffset + textToHighlight.length
+        return Pair(startOffset, endOffset)
+    }
 }
 
 @Service
@@ -28,29 +46,29 @@ class PromptParserService {
     fun highlighter(textField: EditorTextField, prompt: String): EditorTextField {
         val editor = textField.editor
         var markup: MarkupModel? = null
-        if (editor != null){
+        if (editor != null) {
             markup = textField.editor!!.markupModel
             markup.removeAllHighlighters()
         }
 
         PROMPT_KEYWORD.values().forEach {
-            val textToHighlight = "\$${it.text}"
-            if(prompt.contains(textToHighlight)){
-                val startOffset = prompt.indexOf(textToHighlight)
-                val endOffset = startOffset + textToHighlight.length
+
+            it.getOffsets(prompt)?.let { offsets ->
+                val startOffset = offsets.first
+                val endOffset = offsets.second
 
                 if (editor != null) {
                     markup!!.addRangeHighlighter(
                         startOffset, endOffset, HighlighterLayer.LAST,
                         attributes, HighlighterTargetArea.EXACT_RANGE
                     )
-                }else{
-                    textField.addSettingsProvider {textFieldSettings ->
-                    textFieldSettings.markupModel.addRangeHighlighter(
-                        startOffset, endOffset, HighlighterLayer.LAST,
-                        attributes, HighlighterTargetArea.EXACT_RANGE
-                    )
-                }
+                } else {
+                    textField.addSettingsProvider { textFieldSettings ->
+                        textFieldSettings.markupModel.addRangeHighlighter(
+                            startOffset, endOffset, HighlighterLayer.LAST,
+                            attributes, HighlighterTargetArea.EXACT_RANGE
+                        )
+                    }
                 }
             }
         }
@@ -65,7 +83,7 @@ class PromptParserService {
     fun isPromptValid(prompt: String): Boolean {
 
         PROMPT_KEYWORD.values().forEach {
-            if (it.mandatory){
+            if (it.mandatory) {
                 val text = "\$${it.text}"
                 if (!prompt.contains(text))
                     return false
