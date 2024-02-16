72fcaed285ce8a3bcde68c629e6c5f0966ee8544
Vladislav Artiukhov <vladislav.artiukhov@jetbrains.com>
2024-02-16 12:46:55 +0100

    Create a module template and include it in root settings gradle

---
+++ PromptGenerator/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "org.jetbrains.research"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(19)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,6 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
 rootProject.name = "TestSpark"
 include("JUnitRunner")
+include("PromptGenerator")
