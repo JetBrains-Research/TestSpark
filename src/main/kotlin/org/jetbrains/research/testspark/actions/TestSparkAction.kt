77fc6665a5460af363c3303f821e10fa1fdba801
Arkadii Sapozhnikov <arkadii.sapozhnikov@jetbrains.com>
2024-03-06 19:05:45 +0100

    ktlint

--- src/main/kotlin/org/jetbrains/research/testspark/actions/TestSparkAction.kt
+++ src/main/kotlin/org/jetbrains/research/testspark/actions/TestSparkAction.kt
@@ -6,7 +6,6 @@ import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.editor.Caret
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.LibraryOrderEntry
 import com.intellij.openapi.roots.ModuleRootManager
