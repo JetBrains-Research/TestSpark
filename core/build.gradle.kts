5ba11857804c54dc283c810a5f78a1e3208a2adf
Nikolai Sviridov <nikolai.sviridov@jetbrains.com>
2024-03-06 20:50:12 +0100

    feat: add core lib publish

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,8 +1,13 @@
 plugins {
     kotlin("jvm")
+    `maven-publish`
 }
 
 group = "org.jetbrains.research"
+val spaceUsername =
+  System.getProperty("space.username")?.toString() ?: project.properties["spaceUsername"]?.toString() ?: ""
+val spacePublish =
+  System.getProperty("space.publish")?.toString() ?: project.properties["spacePublish"]?.toString() ?: ""
 
 repositories {
     mavenCentral()
@@ -18,4 +23,24 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(17)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = group as String
+            artifactId = "testspark-core"
+            version = "0.0.1"
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            url = uri("https://packages.jetbrains.team/maven/p/automatically-generating-unit-tests/public")
+            credentials {
+                username = spaceUsername
+                password = spacePublish
+            }
+        }
+    }
 }
