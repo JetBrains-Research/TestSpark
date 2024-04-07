116bffb7beca771156e8fce4d74e4dba8594bd17
Vladislav Artiukhov <vladislav.artiukhov@jetbrains.com>
2024-04-07 14:25:22 +0200

    Integrate JarLibraryDescriptor into JUnitVersion and LibraryPathsProvider

--- src/main/kotlin/org/jetbrains/research/testspark/tools/LibraryPathsProvider.kt
+++ src/main/kotlin/org/jetbrains/research/testspark/tools/LibraryPathsProvider.kt
@@ -2,6 +2,7 @@ package org.jetbrains.research.testspark.tools
 
 import com.intellij.openapi.application.PathManager
 import org.jetbrains.research.testspark.core.data.JUnitVersion
+import org.jetbrains.research.testspark.core.test.data.dependencies.JavaTestCompilationDependencies
 import java.io.File
 
 
@@ -16,14 +17,13 @@ class LibraryPathsProvider {
         private val sep = File.separatorChar
         private val libPrefix = "${PathManager.getPluginsPath()}${sep}TestSpark${sep}lib${sep}"
 
-        fun getTestCompilationLibraryPaths() = listOf(
-            "$libPrefix${sep}mockito-core-5.0.0.jar",
-            "$libPrefix${sep}hamcrest-core-1.3.jar",
-            "$libPrefix${sep}byte-buddy-1.14.6.jar",
-            "$libPrefix${sep}byte-buddy-agent-1.14.6.jar",
-        )
+        fun getTestCompilationLibraryPaths() = JavaTestCompilationDependencies.getJarDescriptors().map { descriptor ->
+            "$libPrefix${sep}${descriptor.name}"
+        }
 
-        fun getJUnitLibraryPaths(junitVersion: JUnitVersion): List<String> = junitVersion.libJar.map { "$libPrefix${sep}$it" }
+        fun getJUnitLibraryPaths(junitVersion: JUnitVersion): List<String> = junitVersion.libJar.map { descriptor ->
+            "$libPrefix${sep}${descriptor.name}"
+        }
 
         fun getJacocoCliLibraryPath() = "$libPrefix${sep}jacococli.jar"
         fun getJacocoAgentLibraryPath() = "$libPrefix${sep}jacocoagent.jar"
