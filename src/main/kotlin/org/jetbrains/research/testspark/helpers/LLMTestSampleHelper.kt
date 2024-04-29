32cc5a9940c9c7a16134f1dcff77f42eafb2feab
Arkadii Sapozhnikov <arkadii.sapozhnikov@jetbrains.com>
2024-04-29 19:35:51 +0200

    ktlint

--- src/main/kotlin/org/jetbrains/research/testspark/helpers/LLMTestSampleHelper.kt
+++ src/main/kotlin/org/jetbrains/research/testspark/helpers/LLMTestSampleHelper.kt
@@ -55,9 +55,9 @@ object LLMTestSampleHelper {
         var normalizedImports = imports
         if (normalizedImports.isNotBlank()) normalizedImports += "\n\n"
         return normalizedImports +
-                "public class TestSample {\n" +
-                "   $methodCode\n" +
-                "}"
+            "public class TestSample {\n" +
+            "   $methodCode\n" +
+            "}"
     }
 
     private fun createMethodName(psiClass: PsiClass, method: PsiMethod): String =
