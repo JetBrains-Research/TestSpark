359d8dc22f4c7ccf38ab0f7eb59efd2e7e97f039
Pouria Derakhshanfar <pouria.derakhshanfar@gmail.com>
2024-05-03 17:25:03 +0200

    Fix ktlint issue

--- core/src/main/kotlin/org/jetbrains/research/testspark/core/generation/llm/Utils.kt
+++ core/src/main/kotlin/org/jetbrains/research/testspark/core/generation/llm/Utils.kt
@@ -6,7 +6,7 @@ import org.jetbrains.research.testspark.core.monitor.ErrorMonitor
 import org.jetbrains.research.testspark.core.progress.CustomProgressIndicator
 import org.jetbrains.research.testspark.core.test.TestsAssembler
 import org.jetbrains.research.testspark.core.test.data.TestSuiteGeneratedByLLM
-import java.util.*
+import java.util.Locale
 
 // TODO: find a better place for the below functions
 
