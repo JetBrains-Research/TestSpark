c7e4e7065520036c04ef142d6e5c6cceb3742f82
Arkadii Sapozhnikov <arkadii.sapozhnikov@jetbrains.com>
2024-05-17 22:32:57 +0200

    fix: rename all incorrect factories

--- src/main/kotlin/org/jetbrains/research/testspark/display/utils/ReportUpdater.kt
+++ src/main/kotlin/org/jetbrains/research/testspark/display/utils/ReportUpdater.kt
@@ -2,52 +2,52 @@ package org.jetbrains.research.testspark.display.utils
 
 import org.jetbrains.research.testspark.core.data.Report
 import org.jetbrains.research.testspark.core.data.TestCase
-import org.jetbrains.research.testspark.display.coverage.CoverageVisualisationTabFactory
+import org.jetbrains.research.testspark.display.coverage.CoverageVisualisationTabBuilder
 import org.jetbrains.research.testspark.display.generatedTestsTab.GeneratedTestsTabData
 
 object ReportUpdater {
     fun updateTestCase(
         report: Report,
         testCase: TestCase,
-        coverageVisualisationTabFactory: CoverageVisualisationTabFactory,
+        coverageVisualisationTabBuilder: CoverageVisualisationTabBuilder,
         generatedTestsTabData: GeneratedTestsTabData,
     ) {
         report.testCaseList.remove(testCase.id)
         report.testCaseList[testCase.id] = testCase
         report.normalized()
-        coverageVisualisationTabFactory.show(report, generatedTestsTabData)
+        coverageVisualisationTabBuilder.show(report, generatedTestsTabData)
     }
 
     fun removeTestCase(
         report: Report,
         testCase: TestCase,
-        coverageVisualisationTabFactory: CoverageVisualisationTabFactory,
+        coverageVisualisationTabBuilder: CoverageVisualisationTabBuilder,
         generatedTestsTabData: GeneratedTestsTabData,
     ) {
         report.testCaseList.remove(testCase.id)
         report.normalized()
-        coverageVisualisationTabFactory.show(report, generatedTestsTabData)
+        coverageVisualisationTabBuilder.show(report, generatedTestsTabData)
     }
 
     fun unselectTestCase(
         report: Report,
         testCaseId: Int,
-        coverageVisualisationTabFactory: CoverageVisualisationTabFactory,
+        coverageVisualisationTabBuilder: CoverageVisualisationTabBuilder,
         generatedTestsTabData: GeneratedTestsTabData,
     ) {
         generatedTestsTabData.unselectedTestCases[testCaseId] = report.testCaseList[testCaseId]!!
-        removeTestCase(report, report.testCaseList[testCaseId]!!, coverageVisualisationTabFactory, generatedTestsTabData)
+        removeTestCase(report, report.testCaseList[testCaseId]!!, coverageVisualisationTabBuilder, generatedTestsTabData)
     }
 
     fun selectTestCase(
         report: Report,
         testCaseId: Int,
-        coverageVisualisationTabFactory: CoverageVisualisationTabFactory,
+        coverageVisualisationTabBuilder: CoverageVisualisationTabBuilder,
         generatedTestsTabData: GeneratedTestsTabData,
     ) {
         report.testCaseList[testCaseId] = generatedTestsTabData.unselectedTestCases[testCaseId]!!
         generatedTestsTabData.unselectedTestCases.remove(testCaseId)
         report.normalized()
-        coverageVisualisationTabFactory.show(report, generatedTestsTabData)
+        coverageVisualisationTabBuilder.show(report, generatedTestsTabData)
     }
 }
