fc1c09d25f1eb21ef62056ca1b0a45aadc90059e
Pouria.Derakhshanfar <Pouria.Derakhshanfar@jetbrains.com>
2024-05-27 18:34:57 +0200

    Implements the first version of AppStarter for test generation. ToDo: Running generated tests by Jacoco

---
+++ src/main/kotlin/org/jetbrains/research/testspark/appstarter/TestSparkStarter.kt
@@ -0,0 +1,200 @@
+package org.jetbrains.research.testspark.appstarter
+
+import com.intellij.ide.impl.ProjectUtil
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.ApplicationStarter
+import com.intellij.openapi.components.service
+import com.intellij.openapi.project.DumbService
+import com.intellij.openapi.project.ProjectManager
+import com.intellij.openapi.roots.ProjectFileIndex
+import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiJavaFile
+import com.intellij.psi.PsiManager
+import kotlinx.serialization.ExperimentalSerializationApi
+import org.jetbrains.research.testspark.bundles.llm.LLMDefaultsBundle
+import org.jetbrains.research.testspark.core.data.TestGenerationData
+import org.jetbrains.research.testspark.core.monitor.DefaultErrorMonitor
+import org.jetbrains.research.testspark.data.CodeType
+import org.jetbrains.research.testspark.data.FragmentToTestData
+import org.jetbrains.research.testspark.data.ProjectContext
+import org.jetbrains.research.testspark.data.llm.JsonEncoding
+import org.jetbrains.research.testspark.progress.HeadlessProgressIndicator
+import org.jetbrains.research.testspark.services.LLMSettingsService
+import org.jetbrains.research.testspark.services.PluginSettingsService
+import org.jetbrains.research.testspark.tools.llm.Llm
+import java.io.File
+import kotlin.system.exitProcess
+
+/**
+ * This class represents a test spark starter that implements the ApplicationStarter interface.
+ * It is responsible for generating and running tests based on the provided arguments in headless mode.
+ */
+class TestSparkStarter : ApplicationStarter {
+    @Deprecated("Specify it as `id` for extension definition in a plugin descriptor")
+    override val commandName: String = "testspark"
+
+    /** Sets main (start) thread for IDE in headless as not edt. */
+    override val requiredModality: Int = ApplicationStarter.NOT_IN_EDT
+
+    @Suppress("TooGenericExceptionCaught")
+    @OptIn(ExperimentalSerializationApi::class)
+    override fun main(args: List<String>) {
+        // Project path
+        val projectPath = args[1]
+        // Path to the target file (.java file)
+        val filePath = "$projectPath/${args[2]}"
+        // CUT name (<package-name>.<class-name>)
+        val classUnderTestName = args[3]
+        // Paths to compilation output of the project under test (seperated by ':')
+        val classPath = "$projectPath:${args[4]}"
+        // Selected mode
+        val model = args[5]
+        // Token
+        val token = args[6]
+        // A txt file containing the prompt template
+        val promptTemplateFile = args[7]
+        // Output directory
+        val output = args[8]
+
+        println("Test generation requested for $projectPath")
+
+        ApplicationManager.getApplication().invokeAndWait {
+            val project = ProjectUtil.openOrImport(projectPath, null, true) ?: run {
+                println("couldn't find project in $projectPath")
+                exitProcess(1)
+            }
+            println("Detected project: $project")
+            // Continue when the project is indexed
+            println("Indexing project...")
+            project.let {
+                DumbService.getInstance(it).runWhenSmart {
+                    // open target file
+                    val virtualFile = LocalFileSystem.getInstance().findFileByPath(filePath) ?: run {
+                        println("couldn't open file $filePath")
+                        exitProcess(1)
+                    }
+
+                    // get target PsiClass
+                    val psiFile = PsiManager.getInstance(project).findFile(virtualFile) as PsiJavaFile
+                    val targetPsiClass = detectPsiClass(psiFile.classes, classUnderTestName) ?: run {
+                        println("couldn't find $classUnderTestName in $filePath")
+                        exitProcess(1)
+                    }
+
+                    println("PsiClass ${targetPsiClass.qualifiedName} is detected! Start the test generation process.")
+
+                    // update settings
+                    val settingsState = project.getService(LLMSettingsService::class.java).state
+                    settingsState.currentLLMPlatformName =  LLMDefaultsBundle.get("grazieName")
+                    settingsState.grazieToken = token
+                    settingsState.grazieModel = model
+                    settingsState.classPrompts = JsonEncoding.encode(mutableListOf(File(promptTemplateFile).readText()))
+                    project.service<PluginSettingsService>().state.buildPath = classPath
+
+                    // Prepare Project Context
+                    // First, get CUT Module
+                    val cutModule =
+                        ProjectFileIndex.getInstance(project)
+                            .getModuleForFile(targetPsiClass.containingFile.virtualFile)!!
+                    // Then, instantiate the project context
+                    val projectContext = ProjectContext(
+                        classPath,
+                        output,
+                        targetPsiClass,
+                        targetPsiClass.qualifiedName,
+                        cutModule
+                    )
+                    // Prepare the test generation data
+
+//                    generatedTestsData.baseDir = "${testResultDirectory}$testResultName-validation"
+//                    generatedTestsData.testResultName = testResultName
+                    val testGenerationData =  TestGenerationData(
+                        resultPath = output,
+                        testResultName = "HeadlessGeneratedTests"
+                    )
+                    println("Indexing is done")
+
+                    // get package name
+                    val packageList = targetPsiClass.qualifiedName.toString().split(".").toMutableList()
+                    packageList.removeLast()
+                    val packageName = packageList.joinToString(".")
+
+                    // Create a process Manager
+                    val llmProcessManager = Llm()
+                        .getLLMProcessManager(
+                            project,
+                            psiFile,
+                            targetPsiClass.textRange.startOffset,
+                            "")
+                    // Start test generation
+                    val uiContext = llmProcessManager.runTestGenerator(
+                        HeadlessProgressIndicator(),
+                        FragmentToTestData(CodeType.CLASS),
+                        packageName,
+                        projectContext,
+                        testGenerationData,
+                        DefaultErrorMonitor()
+                        )
+
+
+
+                    // Run test file
+                    // ToDO: run tests
+//                    runTests(project, output, packageList, classPath)
+
+                    ProjectManager.getInstance().closeAndDispose(project)
+
+                    println("[TestSpark Starter] Exiting the headless mode")
+                    exitProcess(0)
+                }
+            }
+        }
+    }
+
+//    private fun runTests(project: Project, out: String, packageList: MutableList<String>, classPath: String) {
+//        val targetDirectory = "$out${File.separator}${packageList.joinToString(File.separator)}"
+//        println("Run tests in $targetDirectory")
+//        File(targetDirectory).walk().forEach {
+//            if (it.name.endsWith(".class")) {
+//                println("Running test ${it.name}")
+//                var testcaseName = it.nameWithoutExtension.removePrefix("Generated")
+//                testcaseName = testcaseName[0].lowercaseChar() + testcaseName.substring(1)
+//                // Test is compiled and it is ready to run jacoco
+//                val testExecutionError = project.service<TestStorageProcessingService>().createXmlFromJacoco(
+//                    it.nameWithoutExtension,
+//                    "$targetDirectory${File.separator}jacoco-${it.nameWithoutExtension}",
+//                    testcaseName,
+//                    classPath,
+//                    packageList.joinToString("."),
+//                    out,
+//                )
+//
+//                saveException(testcaseName, targetDirectory, testExecutionError)
+//            }
+//        }
+//    }
+
+    private fun saveException(
+        testcaseName: String,
+        targetDirectory: String,
+        testExecutionError: String,
+    ) {
+        if (testExecutionError.isBlank() || !testExecutionError.contains("Exception", ignoreCase = false)) {
+            return
+        }
+        val targetPath = "$targetDirectory/$testcaseName-exception.log"
+
+        // Save the exception
+        File(targetPath).writeText(testExecutionError.replace("\tat ", "\nat "))
+    }
+
+    private fun detectPsiClass(classes: Array<PsiClass>, classUnderTestName: String): PsiClass? {
+        for (psiClass in classes) {
+            if (psiClass.qualifiedName == classUnderTestName) {
+                return psiClass
+            }
+        }
+        return null
+    }
+}
