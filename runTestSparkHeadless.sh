3d99e6f943a9538bb2acb6b799616d8e62272110
Pouria.Derakhshanfar <Pouria.Derakhshanfar@jetbrains.com>
2024-05-29 21:36:22 +0200

    Improve error messaging and script readability in runTestSparkHeadless.sh

--- runTestSparkHeadless.sh
+++ runTestSparkHeadless.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # https://stackoverflow.com/a/246128
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 if uname -s | grep -iq cygwin; then
@@ -9,10 +11,20 @@ echo $DIR
 echo "Provided arguments are $@"
 
 if [ $# -ne "11" ]; then
-  echo "$# arguments provided"
-  echo "needs 10 arguments with the following order: 1- project under test root path 2- CUT-File-directory, 3-CUT qualified name, 4-Classpaths containing the compiled project, 5- JUnitVersion ('4' or '5'), 6-Model name, 7-Grazie toke 8-.txt file containing prompt template 9- output directory 10-Space username 11-Space password"
+  echo "$# arguments provided, expected 10 arguments in the following order:
+        1) Path to the root directory of the project under test (ProjectPath)
+        2) Path to the target file (.java file) (it MUST be relative to the ProjectPath)
+        3) Qualified name of the class under test (i.e., <package-name>.<class-name>)
+        4) Classpaths containing the compiled project (separated by ':')
+        5) Version of JUnit testing framework (either 4 or 5)
+        6) Model name (e.g., GPT-4)
+        7) Grazie token
+        8) Filepath to a txt-file containing prompt template
+        9) Output directory
+        10) Space username
+        11) Space password"
   exit 1
 fi
 
 
-"$DIR/gradlew" -p "$DIR" headless -Proot="$1" -Pfile="$2" -Pcut="$3" -Pcp="$4" -Pjunitv="$5" -Pllm="$6" -Ptoken="$7" -Pprompt="$8" -Pout="$9" -Dspace.username="$10" -Dspace.pass="${11}"
+"$DIR/gradlew" -p "$DIR" headless -Proot="$1" -Pfile="$2" -Pcut="$3" -Pcp="$4" -Pjunitv="$5" -Pllm="$6" -Ptoken="$7" -Pprompt="$8" -Pout="$9" -Dspace.username="${10}" -Dspace.pass="${11}"
