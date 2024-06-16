1d04409c203e0295a6149d6c4ac826f7a52d3ea9
Braun Ekaterina <katyabraun0@gmail.com>
2024-06-16 21:59:16 +0200

    added gradle

---
+++ langwrappers/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    kotlin("jvm")
+    id("org.jetbrains.intellij")
+}
+
+repositories {
+    mavenCentral()
+    // Add any other repositories you need
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+
+    implementation(project(":core"))
+}
+
+intellij {
+    version.set("2024.1")
+    plugins.set(listOf("java"))
+    downloadSources.set(true)
+}
+
+kotlin {
+    jvmToolchain(17)
+}
