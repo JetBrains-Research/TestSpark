2bf820078558ad52a0d452d2505d48bc61ca36de
Braun Ekaterina <katyabraun0@gmail.com>
2024-06-27 13:15:31 +0200

    feat added strategy pattern

--- java/src/main/kotlin/org/jetbrains/research/testspark/java/JavaPsiClassWrapper.kt
+++ java/src/main/kotlin/org/jetbrains/research/testspark/java/JavaPsiClassWrapper.kt
@@ -10,10 +10,9 @@ import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.search.searches.ClassInheritorsSearch
 import com.intellij.psi.util.PsiTypesUtil
 import org.jetbrains.research.testspark.core.data.ClassType
-import org.jetbrains.research.testspark.core.utils.importPattern
-import org.jetbrains.research.testspark.core.utils.packagePattern
 import org.jetbrains.research.testspark.langwrappers.PsiClassWrapper
 import org.jetbrains.research.testspark.langwrappers.PsiMethodWrapper
+import org.jetbrains.research.testspark.langwrappers.strategies.JavaKotlinFullTextExtractionStrategy
 
 class JavaPsiClassWrapper(private val psiClass: PsiClass) : PsiClassWrapper {
     override val name: String get() = psiClass.name ?: ""
@@ -33,29 +32,10 @@ class JavaPsiClassWrapper(private val psiClass: PsiClass) : PsiClassWrapper {
     override val containingFile: PsiFile get() = psiClass.containingFile
 
     override val fullText: String
-        get() {
-            var fullText = ""
-            val fileText = psiClass.containingFile.text
-
-            // get package
-            packagePattern.findAll(fileText).map {
-                it.groupValues[0]
-            }.forEach {
-                fullText += "$it\n\n"
-            }
-
-            // get imports
-            importPattern.findAll(fileText).map {
-                it.groupValues[0]
-            }.forEach {
-                fullText += "$it\n"
-            }
-
-            // Add class code
-            fullText += psiClass.text
-
-            return fullText
-        }
+        get() = JavaKotlinFullTextExtractionStrategy.extract(
+            psiClass.containingFile,
+            psiClass.text,
+        )
 
     override val classType: ClassType
         get() {
