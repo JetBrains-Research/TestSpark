d6bed007512dc9b3b2eb3524e85a4cc405ca70f7
Edwin Fernando <edwin1729@vivaldi.net>
2024-07-24 11:55:08 +0100

    remove settings for unsupported kex features

--- src/main/kotlin/org/jetbrains/research/testspark/services/KexSettingsService.kt
+++ src/main/kotlin/org/jetbrains/research/testspark/services/KexSettingsService.kt
@@ -14,10 +14,22 @@ import org.jetbrains.research.testspark.settings.kex.KexSettingsState
 @State(name = "KexSettingsState", storages = [Storage("KexSettings.xml")])
 class KexSettingsService : PersistentStateComponent<KexSettingsState> {
     private var kexSettingsState: KexSettingsState = KexSettingsState()
+
+    /**
+     * Gets the currently persisted state of the application.
+     * This method is called every time the values in the EvoSuite Settings page are saved.
+     * If the values from getState are different from the default values obtained by calling
+     *  the default constructor, the state is persisted (serialised and stored).
+     */
     override fun getState(): KexSettingsState {
         return kexSettingsState
     }
 
+    /**
+     * Loads the state of the application-level settings.
+     * This method is called after the application-level settings component has been created
+     *   and if the XML file with the state is changes externally.
+     */
     override fun loadState(state: KexSettingsState) {
         kexSettingsState = state
     }
