4c3785ecebdc6282d65ee04ab0b3422dd6844522
Arkadii Sapozhnikov <arkadii.sapozhnikov@jetbrains.com>
2024-09-02 16:17:19 +0200

    add empty LLM response handling

--- src/main/kotlin/org/jetbrains/research/testspark/display/generatedTests/TestCasePanelBuilder.kt
+++ src/main/kotlin/org/jetbrains/research/testspark/display/generatedTests/TestCasePanelBuilder.kt
@@ -19,6 +19,7 @@ import com.intellij.ui.JBColor
 import com.intellij.ui.LanguageTextField
 import com.intellij.ui.components.JBScrollPane
 import com.intellij.util.ui.JBUI
+import org.jetbrains.research.testspark.bundles.llm.LLMMessagesBundle
 import org.jetbrains.research.testspark.bundles.plugin.PluginLabelsBundle
 import org.jetbrains.research.testspark.bundles.plugin.PluginMessagesBundle
 import org.jetbrains.research.testspark.core.data.Report
@@ -45,6 +46,7 @@ import org.jetbrains.research.testspark.tools.TestProcessor
 import org.jetbrains.research.testspark.tools.TestsExecutionResultManager
 import org.jetbrains.research.testspark.tools.ToolUtils
 import org.jetbrains.research.testspark.tools.factories.TestCompilerFactory
+import org.jetbrains.research.testspark.tools.llm.error.LLMErrorManager
 import org.jetbrains.research.testspark.tools.llm.test.JUnitTestSuitePresenter
 import java.awt.Dimension
 import java.awt.Toolkit
@@ -429,7 +431,9 @@ class TestCasePanelBuilder(
                         uiContext.errorMonitor,
                     )
 
-                    if (modifiedTest != null) {
+                    if (modifiedTest == null || modifiedTest.testCases.isEmpty()) {
+                        LLMErrorManager().warningProcess(LLMMessagesBundle.get("modifyWithLLMError"), project)
+                    } else {
                         modifiedTest.setTestFileName(
                             getClassWithTestCaseName(testCase.testName),
                         )
