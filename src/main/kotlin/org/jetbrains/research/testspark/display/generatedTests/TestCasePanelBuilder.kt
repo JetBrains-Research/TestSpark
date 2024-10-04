45588f6aa72b1e3e77ea9b01e08ceb64894d76c2
Vladislav Artiukhov <vladislav.artiukhov@jetbrains.com>
2024-10-04 12:35:59 +0200

    fix: updated the link to the discussion

--- src/main/kotlin/org/jetbrains/research/testspark/display/generatedTests/TestCasePanelBuilder.kt
+++ src/main/kotlin/org/jetbrains/research/testspark/display/generatedTests/TestCasePanelBuilder.kt
@@ -308,7 +308,7 @@ class TestCasePanelBuilder(
 
         /**
          * The following code is a workaround for the issue with the ComboBox preferred size.
-         * See: https://github.com/JetBrains-Research/TestSpark/pull/343/files#r1781430743
+         * See: https://github.com/JetBrains-Research/TestSpark/pull/343#discussion_r1781430743
          */
         requestComboBox.preferredSize = Dimension(0, 0)
         requestComboBox.isEditable = true
