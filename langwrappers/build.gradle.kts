efe28042a0b67c35446a38905aa973c5f010fbaa
Pouria.Derakhshanfar <Pouria.Derakhshanfar@jetbrains.com>
2024-10-11 11:45:04 +0200

    Update other modules' build.gradle.kts

--- langwrappers/build.gradle.kts
+++ langwrappers/build.gradle.kts
@@ -1,26 +1,42 @@
 plugins {
     kotlin("jvm")
-    id("org.jetbrains.intellij")
+    id("org.jetbrains.intellij.platform")
 }
 
 repositories {
     mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
 dependencies {
+
+    intellijPlatform {
+        create(rootProject.properties["platformType"].toString(), rootProject.properties["platformVersion"].toString())
+        // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
+        bundledPlugins(listOf("com.intellij.java"))
+
+        instrumentationTools()
+    }
     implementation(kotlin("stdlib"))
 
     implementation(project(":core"))
 }
 
-intellij {
-    rootProject.properties["platformVersion"]?.let { version.set(it.toString()) }
-    plugins.set(listOf("java"))
+intellijPlatform {
+    pluginConfiguration {
+        rootProject.properties["platformVersion"]?.let { version = it.toString() }
+    }
+//    apply(plugin = "java")
+//    // Apply more plugins if necessary
+//    apply(plugin = "kotlin")
 }
 
 tasks.named("verifyPlugin") { enabled = false }
 tasks.named("runIde") { enabled = false }
-tasks.named("runPluginVerifier") { enabled = false }
+tasks.named("prepareJarSearchableOptions") { enabled = false }
+//tasks.named("runPluginVerifier") { enabled = false }
 
 kotlin {
     jvmToolchain(rootProject.properties["jvmToolchainVersion"].toString().toInt())
