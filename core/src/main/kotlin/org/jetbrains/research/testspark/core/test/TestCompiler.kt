51b9357100d4d77f7b8daa7fe26893a10c456824
Iurii Zaitsev <iurii.zaitsev@jetbrains.com>
2024-10-11 21:02:42 +0200

    Comments update

--- core/src/main/kotlin/org/jetbrains/research/testspark/core/test/TestCompiler.kt
+++ core/src/main/kotlin/org/jetbrains/research/testspark/core/test/TestCompiler.kt
@@ -20,6 +20,7 @@ abstract class TestCompiler(libPaths: List<String>, junitLibPaths: List<String>)
      * @param generatedTestCasesPaths A list of file paths where the generated test cases are located.
      * @param buildPath All the directories where the compiled code of the project under test is saved. This path is used as a classpath to run each test case.
      * @param testCases A mutable list of `TestCaseGeneratedByLLM` objects representing the test cases to be compiled.
+     * @param workingDir The path of the directory that contains package directories of the code to compile
      * @return A `TestCasesCompilationResult` object containing the overall compilation success status and a set of compilable test cases.
      */
     fun compileTestCases(
@@ -47,6 +48,7 @@ abstract class TestCompiler(libPaths: List<String>, junitLibPaths: List<String>)
      *
      * @param path The path of the code file to compile.
      * @param projectBuildPath All the directories where the compiled code of the project under test is saved. This path is used as a classpath to run each test case.
+     * @param workingDir The path of the directory that contains package directories of the code to compile
      * @return A pair containing a boolean value indicating whether the compilation was successful (true) or not (false),
      *         and a string message describing any error encountered during compilation.
      */
