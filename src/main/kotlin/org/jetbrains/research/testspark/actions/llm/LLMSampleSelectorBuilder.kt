02007590a1bf3c77fa7da8df2e0573c7a669f11a
Stephan Lukasczyk <stephan@lukasczyk.name>
2024-11-21 10:58:10 +0100

    Sample test-code candidates based on current open file
    Previously, all tests from a project where incorporated as test-code
    candidates for the LLM, however, this can be slow for larger projects.
    This change uses the currently open file, determines the test methods
    that call code in the currently open file, and only use those as
    candidates for the LLM.  As a fallback, if none could be found, all
    tests from the project will be candidates.

--- src/main/kotlin/org/jetbrains/research/testspark/actions/llm/LLMSampleSelectorBuilder.kt
+++ src/main/kotlin/org/jetbrains/research/testspark/actions/llm/LLMSampleSelectorBuilder.kt
@@ -1,5 +1,7 @@
 package org.jetbrains.research.testspark.actions.llm
 
+import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.fileTypes.FileType
 import com.intellij.openapi.fileTypes.FileTypeManager
 import com.intellij.openapi.project.Project
@@ -9,6 +11,9 @@ import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiJavaFile
 import com.intellij.psi.PsiManager
 import com.intellij.psi.PsiMethod
+import com.intellij.psi.search.ProjectAndLibrariesScope
+import com.intellij.psi.search.searches.ReferencesSearch
+import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.util.containers.stream
 import com.intellij.util.ui.FormBuilder
 import org.jetbrains.research.testspark.actions.template.PanelBuilder
@@ -32,7 +37,7 @@ class LLMSampleSelectorBuilder(private val project: Project, private val languag
 
     private val defaultTestName = "<html>provide manually</html>"
     private val defaultTestCode = "// provide test method code here"
-    private val testNames = mutableListOf(defaultTestName)
+    private val testNames = mutableSetOf(defaultTestName)
     private val initialTestCodes = mutableListOf(createTestSampleClass("", defaultTestCode))
     private val testSamplePanelFactories: MutableList<TestSamplePanelBuilder> = mutableListOf()
     private var testSamplesCode: String = ""
@@ -54,7 +59,7 @@ class LLMSampleSelectorBuilder(private val project: Project, private val languag
     init {
         addListeners()
 
-        collectTestSamples(project, testNames, initialTestCodes)
+        collectTestSamples()
     }
 
     override fun getTitlePanel(): JPanel {
@@ -138,7 +143,7 @@ class LLMSampleSelectorBuilder(private val project: Project, private val languag
 
         addButton.addActionListener {
             val testSamplePanelBuilder =
-                TestSamplePanelBuilder(project, middlePanel, testNames, initialTestCodes, language)
+                TestSamplePanelBuilder(project, middlePanel, testNames.toMutableList(), initialTestCodes, language)
             testSamplePanelFactories.add(testSamplePanelBuilder)
             val testSamplePanel = testSamplePanelBuilder.getTestSamplePanel()
             val codeScrollPanel = testSamplePanelBuilder.getCodeScrollPanel()
@@ -183,12 +188,70 @@ class LLMSampleSelectorBuilder(private val project: Project, private val languag
         }
     }
 
+    private fun collectTestSamples() {
+        collectTestSamplesForCurrentFile()
+
+        if (testNames.size == 1) {
+            // Only the default test name is there, thus we did not find any tests related to the
+            // current file; collect all test samples and provide them to the user instead
+            collectTestSamples(project)
+        }
+    }
+
+    private fun collectTestSamplesForCurrentFile() {
+        val currentDocument = FileEditorManager.getInstance(project).selectedTextEditor?.document
+        val currentFile = currentDocument?.let { FileDocumentManager.getInstance().getFile(it) }
+        val javaFileType: FileType = FileTypeManager.getInstance().getFileTypeByExtension("java")
+        val projectScope = ProjectAndLibrariesScope(project)
+        if (currentFile!!.fileType === javaFileType) {
+            val psiJavaFile = (PsiManager.getInstance(project).findFile(currentFile!!) as PsiJavaFile)
+            val psiClass = psiJavaFile.classes[
+                psiJavaFile.classes.stream().map { it.name }.toArray()
+                    .indexOf(psiJavaFile.name.removeSuffix(".java"))
+            ]
+            psiClass.methods.forEach { method ->
+                ReferencesSearch.search(method, projectScope).forEach { reference ->
+                    val enclosingMethod = PsiTreeUtil.getParentOfType(
+                        reference.element, PsiMethod::class.java)
+                    if (enclosingMethod != null) {
+                        val enclosingClass = enclosingMethod.containingClass
+                        val enclosingFile = (enclosingMethod.containingFile as PsiJavaFile)
+                        val imports = retrieveImportStatements(enclosingFile, enclosingClass!!)
+                        processCandidateMethod(enclosingMethod, imports, psiClass)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun retrieveImportStatements(psiJavaFile: PsiJavaFile, psiClass: PsiClass): String {
+        var imports = psiJavaFile.importList?.allImportStatements?.map { it.text }?.toList()
+            ?.joinToString("\n") ?: ""
+        if (psiClass.qualifiedName != null && psiClass.qualifiedName!!.contains(".")) {
+            imports += "\nimport ${psiClass.qualifiedName?.substringBeforeLast(".") + ".*"};"
+        }
+        return imports
+    }
+
+    private fun processCandidateMethod(psiMethod: PsiMethod, imports: String, psiClass: PsiClass) {
+        val annotations = psiMethod.annotations
+        annotations.forEach { annotation ->
+            if (annotation.qualifiedName == "org.junit.jupiter.api.Test"
+                    || annotation.qualifiedName == "org.junit.Test") {
+                val code: String = createTestSampleClass(imports, psiMethod.text)
+                if (testNames.add(createMethodName(psiClass, psiMethod))) {
+                    initialTestCodes.add(code)
+                }
+            }
+        }
+    }
+
     /**
      * Retrieves a list of test samples from the given project.
      *
      * @return A list of strings, representing the names of the test samples.
      */
-    private fun collectTestSamples(project: Project, testNames: MutableList<String>, initialTestCodes: MutableList<String>) {
+    private fun collectTestSamples(project: Project) {
         val projectFileIndex: ProjectFileIndex = ProjectRootManager.getInstance(project).fileIndex
         val javaFileType: FileType = FileTypeManager.getInstance().getFileTypeByExtension("java")
 
@@ -200,11 +263,7 @@ class LLMSampleSelectorBuilder(private val project: Project, private val languag
                         psiJavaFile.classes.stream().map { it.name }.toArray()
                             .indexOf(psiJavaFile.name.removeSuffix(".java")),
                     ]
-                    var imports = psiJavaFile.importList?.allImportStatements?.map { it.text }?.toList()
-                        ?.joinToString("\n") ?: ""
-                    if (psiClass.qualifiedName != null && psiClass.qualifiedName!!.contains(".")) {
-                        imports += "\nimport ${psiClass.qualifiedName?.substringBeforeLast(".") + ".*"};"
-                    }
+                    var imports = retrieveImportStatements(psiJavaFile, psiClass)
                     psiClass.allMethods.forEach { method ->
                         val annotations = method.modifierList.annotations
                         annotations.forEach { annotation ->
