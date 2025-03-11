af68172e5f79bc2100327d161d893547c274b39c
Arkadii Sapozhnikov <arkadii.sapozhnikov@jetbrains.com>
2025-03-11 23:00:45 +0100

    remove unused JButtonFixture import

--- src/test/kotlin/org/jetbrains/research/testspark/uiTest/pages/IdeaFrame.kt
+++ src/test/kotlin/org/jetbrains/research/testspark/uiTest/pages/IdeaFrame.kt
@@ -6,7 +6,6 @@ import com.intellij.remoterobot.fixtures.CommonContainerFixture
 import com.intellij.remoterobot.fixtures.ComponentFixture
 import com.intellij.remoterobot.fixtures.DefaultXpath
 import com.intellij.remoterobot.fixtures.FixtureName
-import com.intellij.remoterobot.fixtures.JButtonFixture
 import com.intellij.remoterobot.search.locators.byXpath
 import com.intellij.remoterobot.utils.WaitForConditionTimeoutException
 import com.intellij.remoterobot.utils.keyboard
