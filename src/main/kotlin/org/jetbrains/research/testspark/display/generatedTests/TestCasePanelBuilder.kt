d5251537ad63e5bac72d0652dd2d92fe41f7c389
Arkadii Sapozhnikov <arkadii.sapozhnikov@jetbrains.com>
2025-03-22 00:38:24 +0100

    remove the loading icon

--- src/main/kotlin/org/jetbrains/research/testspark/display/generatedTests/TestCasePanelBuilder.kt
+++ src/main/kotlin/org/jetbrains/research/testspark/display/generatedTests/TestCasePanelBuilder.kt
@@ -157,8 +157,6 @@ class TestCasePanelBuilder(
 
     private val sendButton = IconButtonCreator.getButton(TestSparkIcons.send, PluginLabelsBundle.get("send"))
 
-    private val loadingLabel: JLabel = JLabel(TestSparkIcons.loading)
-
     private val initialCodes: MutableList<String> = mutableListOf()
     private val lastRunCodes: MutableList<String> = mutableListOf()
     private val currentCodes: MutableList<String> = mutableListOf()
@@ -303,8 +301,6 @@ class TestCasePanelBuilder(
         buttonsPanel.add(Box.createRigidArea(Dimension(checkbox.preferredSize.width, checkbox.preferredSize.height)))
         runTestButton.isEnabled = true
         buttonsPanel.add(runTestButton)
-        loadingLabel.isVisible = false
-        buttonsPanel.add(loadingLabel)
         buttonsPanel.add(Box.createHorizontalGlue())
         resetButton.isEnabled = false
         buttonsPanel.add(resetButton)
@@ -511,7 +507,6 @@ class TestCasePanelBuilder(
      * After adding the code, it switches to another code.
      */
     private fun sendRequest() {
-        loadingLabel.isVisible = true
         enableGlobalComponents(false)
         enableLocalComponents(false)
 
@@ -576,7 +571,6 @@ class TestCasePanelBuilder(
 
     private fun finishProcess(enableGlobal: Boolean = true) {
         uiContext.errorMonitor.clear()
-        loadingLabel.isVisible = false
         if (enableGlobal) enableGlobalComponents(true)
         enableLocalComponents(true)
     }
@@ -634,7 +628,6 @@ class TestCasePanelBuilder(
         if (isRemoved) return
         if (!runTestButton.isEnabled) return
 
-        loadingLabel.isVisible = true
         enableGlobalComponents(false)
         enableLocalComponents(false)
 
@@ -653,7 +646,6 @@ class TestCasePanelBuilder(
         if (isRemoved) return
         if (!runTestButton.isEnabled) return
 
-        loadingLabel.isVisible = true
         enableGlobalComponents(false)
         enableLocalComponents(false)
 
