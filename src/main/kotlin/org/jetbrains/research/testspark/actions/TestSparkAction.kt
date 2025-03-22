8a48cdef30325a8f46ec4fcf6297b76b4821b99f
Stephan Lukasczyk <stephan.lukasczyk@jetbrains.com>
2025-03-22 19:50:06 +0100

    Fix ktlint issues

--- src/main/kotlin/org/jetbrains/research/testspark/actions/TestSparkAction.kt
+++ src/main/kotlin/org/jetbrains/research/testspark/actions/TestSparkAction.kt
@@ -31,7 +31,6 @@ class TestSparkAction : AnAction() {
 
     val testsExecutionResultManager = TestsExecutionResultManager()
 
-
     /**
      * Handles the action performed event.
      *
@@ -42,7 +41,6 @@ class TestSparkAction : AnAction() {
      *           This parameter is required.
      */
     override fun actionPerformed(e: AnActionEvent) {
-
         if (currentTestSparkActionWindow != null) {
             currentTestSparkActionWindow?.dispose()
         }
