e3c1bae7408a6d0dfef865a30a8011548a472144
Arkadii Sapozhnikov <arkadii.sapozhnikov@jetbrains.com>
2025-05-28 01:37:46 +0200

    refactor: simplify UserExperienceReport by moving `generationTool` initialization

--- src/main/kotlin/org/jetbrains/research/testspark/collectors/UserExperienceReport.kt
+++ src/main/kotlin/org/jetbrains/research/testspark/collectors/UserExperienceReport.kt
@@ -4,16 +4,14 @@ import org.jetbrains.research.testspark.core.test.data.CodeType
 import org.jetbrains.research.testspark.tools.GenerationTool
 
 class UserExperienceReport(
-    generationTool: GenerationTool,
+    // Tool used in the test generation
+    var generationTool: GenerationTool,
 ) {
     // Test generation id
     var id: String? = null
 
     // Test generation starting time
     var testGenerationStartTime: Long? = null
-
-    // Tool used in the test generation
-    var generationTool: GenerationTool = generationTool
 
     // Code type tested in the test generation
     var codeType: CodeType? = null
